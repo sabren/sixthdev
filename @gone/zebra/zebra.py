7e1b4b0aca2d8af0884c847a35c6af508531267f
sabren <sabren>
2000-02-26 18:10:37 +0000

    renamed 'tail' to 'foot', broke generators off into seperate file...

--- @gone/zebra/zebra.py
+++ @gone/zebra/zebra.py
@@ -31,7 +31,7 @@ class Engine:
     def __init__(self, generator=None):
         self.parser = Parser()
         if generator == None:
-            self.generator = PyGenerator()
+            self.generator = Generator()
         else:
             self.generator = generator
     def compile(self, text):
@@ -173,13 +173,13 @@ class Parser (xmllib.XMLParser):
                 "query": [],
                 "head" : [],
                 "body" : [],
-                "tail" : [],
+                "foot" : [],
                 "none" : [],
                 "grouph": [], # group heads
-                "groupt": [], # group tails
+                "groupf": [], # group feet
                 "groups": [], # groups (fields)
                 "gdepth": 0}  # group depth
-        elif tag in ["query", "head", "body", "tail", "none"]:
+        elif tag in ["query", "head", "body", "foot", "none"]:
             ## then start a new stripe!
             self.datstack.append(self.stripe)
             self.stripe = []
@@ -228,7 +228,7 @@ class Parser (xmllib.XMLParser):
         elif tag == "skin":
             # initialize a new, empty skin:
             self.datstack.append(self.struct)
-            self.struct = {"head": "",  "tail": ""}
+            self.struct = {"head": "",  "foot": ""}
             self.skins[attrs["name"]] = self.struct
         elif tag == "include":
             incfile = attrs["file"]
@@ -255,8 +255,8 @@ class Parser (xmllib.XMLParser):
         self.tagstack = self.tagstack[:-1]
         parent = self.tagstack[-1]
 
-        if tag in ["query", "head", "tail", "body", "none"]:
-            if parent == "group" and tag in ["head", "tail"]:
+        if tag in ["query", "head", "foot", "body", "none"]:
+            if parent == "group" and tag in ["head", "foot"]:
                 ## grouph will look like: [outer, middle, inner]
                 ## groupt will look like: [inner, middle, outer]
                 ## It's just easier that way.
@@ -282,10 +282,10 @@ class Parser (xmllib.XMLParser):
             self.stripe = self.datstack[-1]
             self.datstack = self.datstack[:-1]
         elif tag == "wrap":
-            self.stripe.append(self.skins[self.wrapstack[-1]]["tail"])
+            self.stripe.append(self.skins[self.wrapstack[-1]]["foot"])
             self.wrapstack = self.wrapstack[:-1]
         elif tag == "group":
-            ## not all groups have heads or tails. If this one
+            ## not all groups have heads or feet If this one
             ## doesn't, even out the stack(s) with an empty value
             if len(self.struct["grouph"]) < len(self.struct["groups"]):
                 self.struct["grouph"].append(None)
@@ -298,8 +298,6 @@ class Parser (xmllib.XMLParser):
         else:
             pass # </zebra>, or unknown tag
 
-
-    ###############################################
 
     ###############################################
 
@@ -323,228 +321,44 @@ class Parser (xmllib.XMLParser):
 ###################################################
 ##[ zebra.Generator ]##############################
 ###################################################
+##
+## @TODO: Generator should do MUCH  more of the work
+##
+## I'm thinking that flatten() should be set up more
+## like xmllib... it'll have a flatten_report
+## (which it has now), flatten_xxx, flatten_yyyyyy
+## and then a flatten_unknown which gets called by
+## default.
+##
+## The only reason I'm not doing this now is because
+## I'm waiting for the "official" python DOM
+## implementation to work itself out, at which point
+## I'll want Zebra to use that, and the whole concept
+## of what's being flattened will completely change.
+##
+## besides: I'm almost pretending that the PHP3 stuff
+## isn't there, and all we do is python.. :)
+##
+#####################################################
 class Generator:
     reZVar = re.compile("{([$!]?\w+)}", re.I | re.S )
 
     def __init__(self):
-        self._head = ""
-        self._foot = ""
+        self.head = ""
+        self.foot = ""
     
     def generate(self, parsedict):
-        return self._head + self._flatten(parsedict["stripe"]) + self._foot
-    
-    def _flatten(self, stripeset, depth=0, context="show"):
-        """Converts a stripe or stripeset into a string"""
-
-        res = ""
-        if type(stripeset)!=types.ListType:
-            stripeset = [stripeset]
-
-        for stripe in stripeset:
-            stripehead  = stripebody  = stripetail  = ""
-            test = conditional = ""
-            if type(stripe)==types.StringType:
-                ## strip leading and trailing newlines
-                if stripe[0] == "\n":
-                    stripe = stripe[1:]
-                if stripe[-1] == "\n":
-                    stripe = stripe[:-1]
-                ## deal with context (we only care when it's a string)
-                if (context == "show"):
-                    stripehead = 'print "'
-                    stripebody = string.replace(
-                        string.replace(stripe, '"', '\\"'),
-                        "\n", "\\n")
-                    stripetail = '";'
-                elif context == "exec":
-                    stripebody = stripe
-                else:
-                    #@TODO: this should raise an error
-                    pass
-            elif type(stripe)==types.ListType:
-                stripebody = self.flatten(stripe,depth+1)
-            elif type(stripe)==types.DictionaryType:
-                # @TODO: this ought to all be stored in a
-                # dictionary that maps tags to functions
-                tag = stripe["tag"]
-                if tag == "stripe":
-                    stripebody = self.flatten(stripe["content"],depth+1,
-                                              stripe["context"])[:-1]
-                    test   = stripe["test"]
-                    conditional = stripe["conditional"]
-                elif tag == "report":
-                    res = res + self.flatten_report(stripe, depth+1)
-                else:
-                    print "*** don't know how to flatten " + tag
-            else:
-                raise("unknown structure in stripe")
-
-            ## finally, the conditional
-            if (test) and not (conditional):
-                conditional = "if"
-            if (test) or (conditional): # el has no condition
-                conditionals = {"if":"if", "ef":"elseif", "el":"else"}
-                if conditional == "":
-                    conditional = "if"
-                if conditional in ["if", "ef"]:
-                    stripehead = conditionals[conditional] + " (" \
-                                 + test + "){\n" \
-                                 + "   " + stripehead
-                    stripetail = stripetail + "\n}"
-                elif conditional == "el":
-                    stripehead = conditionals[conditional] + " {" \
-                                 + "   " + stripehead
-                    stripetail = stripetail + "\n}"
-
-            res = res + stripehead + stripebody + stripetail + "\n"
-
-        # now that we have the whole set, interpolate variables:
-        res = self.reZVar.sub(self._interpolate, res)
-        return res
-
-    ###############################################
-
-    def flatten_report(self, report, depth=0):
-
-        # @TODO: this is one serious kludge.
-        # @TODO: code each variable name with depth so can have nested reports
-        res = ""
-        res = res + \
-              "$__db = new " + report["source"] + ";\n" + \
-              "$__db->query(\"" + self.flatten(report["query"], context="exec") + "\");\n"
-
-        ## set up two arrays for handling groups
-        res = res + "$__groups=array('all'"
-        buf = "$__showtail=array(0"
-        for g in report["groups"]:
-            res = res + ", '" + g + "'"
-            buf = buf + ",0"
-        res = res + ");\n"
-        res = res + buf + ");\n"
-
-        ## now do the test:
-        res = res + "if ($__db->next_record()) {\n";
-
-        ## if there's records, show the head:
-        res = res + "   " + self.flatten(report["head"],depth,"show")
-
-        ## nr is next record, tr = this one, pr = previous
-        ## we need to look at 3 records at once in order to
-        ## know when to print the headers and footers..
-        res = res + \
-              "   $__nr = $__db->Record;\n" + \
-              "   while (($__more = $__db->next_record()) or (! $__nomore)){\n" + \
-              "      $__tr = $__nr;\n" + \
-              "      if ($__more) { $__nr = $__db->Record; }\n" + \
-              "      else { $__nomore = 1; }\n"
-
-        ## handle the grouping for the heads:
-        for i in range(len(report["groups"])):
-            if report["grouph"][i]:
-                res = res + \
-                      "      if ($__tr[\"" + report["groups"][i] + "\"] != $__pr[\"" + \
-                      report["groups"][i] + "\"]){\n" + \
-                      "         " + self.flatten(report["grouph"][i],depth,"show") + \
-                      "         unset($__pr);\n" + \
-                      "      }\n"
-
-        ## now the body:
-        res = res + "      " + self.flatten(report["body"],depth,"show")
-
-        ## and the tails:
-        ## .. use a while loop because they're faster than for loops in php3
-        res = res + \
-              "      if ($__nomore) { $__showTail[0] = 1; }\n" + \
-              "      $__g=1; while ($__g < sizeof($__showTail)){ \n" + \
-              "         if (($__nr[$__groups[$__g]] != $__tr[$__groups[$__g]]) " + \
-              "or ($__showTail[$__g-1])){\n" + \
-              "            $__showTail[$__g] = 1;\n" + \
-              "         } else {\n" + \
-              "            $__showTail[$__g] = 0;\n" + \
-              "         }\n" + \
-              "         $__g++;\n" + \
-              "      }\n"
-
-        for i in range(len(report["groups"])):
-            if report["groupt"][i]:
-                res = res + \
-                      "      if ($__showTail[" + `i+1` + "]){\n" + \
-                      "         " + self.flatten(report["groupt"][i],depth,"show") + \
-                      "      }\n"
-
-        ## cap off the loop, show the tail...
-        res = res + \
-              "      $__pr = $__tr;\n" + \
-              "   }\n" + \
-              "   " + self.flatten(report["tail"],depth,"show")
-
-        ## handle "none"
-        if report["none"]:
-            res = res + \
-                  "} else {\n" + \
-                  self.flatten(report["none"],depth,"show")
-        res = res + "}\n"
-
-        ## finally, encode variables with depth, so we can nest reports
-        reDepth = re.compile("(\$__\w+)", re.I | re.S )
-        res = reDepth.sub(r"\1_" + `depth`,res)
-
-        return res
-
-
-
-    ###############################################
-
-    def _interpolate(self, match):
-
-        """replaces {fields}, {$vars} and {!inserts}
-        in a flattened zebra stripeset..."""
-
-        #@TODO: this is all hard-coded for PHP3. make it generic.
-        #@TODO: interpolation needs to be table driven so that
-        # we can translate into different languages
-        # (or maybe an overridable function?)
-        # also, it ought to be per-stripe (and only used on stripe
-        # bodies and conditionals)
-        # .. that way, we can change interpolation as context changes.
-
-        token = match.group(1)
-        if token[0]=="!":
-            if self.named.has_key(token[1:]):
-                # context="exec" so you don't get: print "print"whatever""
-                # @TODO: maybe have a "passthru" context, as some
-                # languages might alter "exec"?
-                # (eg, php3 mode might one day use "<?" and  "?>"
-                return self.flatten(self.named[token[1:]], context="exec")
-            else:
-                return ""
-        elif token[0] == "$":
-            return token
-        else:
-            # fields.. there needs to be a standard
-            # way of doing this per language.
-            return '$__tr[' + token + ']';
-
+        return self.head + self.flatten(parsedict["stripe"]) + self.foot
 
     
+    def flatten(self, stripeset, depth=0, context="show"):
+        return ""
     
-###################################################
-##[ zebra.PHPGenerator ]###########################
-###################################################
-class PHPGenerator(Generator):
-    def __init__(self):
-        self._head = "<?\n"
-        self._foot = "?>\n"
-    
-#    def _flatten(self, stripeset, depth=0, context="show"):
-#        return ""
+    def interpolate(self):
+        pass
 
-
-###################################################
-##[ zebra.PyGenerator ]############################
-###################################################
-class PyGenerator(Generator):
-    pass
+    def flatten_report(self):
+        pass
 
 
 ###################################################
