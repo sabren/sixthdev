b2c7f9cc7042853b9121d58740d97e56c3583b71
sabren <sabren>
2000-04-13 23:09:33 +0000

    *** empty log message ***

---
+++ @dead/zdc/Field.py
@@ -0,0 +1,14 @@
+# zdc.Field
+
+class Field:
+
+    def __init__(self, name, type, displaySize=None, internalSize=None,
+                 precision=None, scale=None, allowNull=None, default=None):
+        self.name = name
+        self.type = type
+        self.displaySize = displaySize
+        self.internalSize = internalSize
+        self.precision = precision
+        self.scale = scale
+        self.allowNull = allowNull
+        self.default = default
