b8045a5d596670ae1567c15f3bd5d58c0b56e161
sabren <sabren>
2000-04-17 21:53:02 +0000

    *** empty log message ***

---
+++ @gone/weblib/test/testUser.py
@@ -0,0 +1,10 @@
+#
+# testUser.py - unit tests for User.py
+#
+
+import unittest
+from weblib import User
+
+class UserTestCase(unittest.TestCase):
+    def checkInt(self):
+        assert 0, "OH NO!"
