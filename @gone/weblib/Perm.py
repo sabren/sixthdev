d0e1268ea88be84023726189aaa39caf50aaa856
sabren <sabren>
2000-04-17 21:53:01 +0000

    *** empty log message ***

---
+++ @gone/weblib/Perm.py
@@ -0,0 +1,35 @@
+"""
+Perm.py - emulate PHPLIB's permission support in python.
+
+@TODO: license
+"""
+
+class Perm:
+
+    ## attributes #############################
+    
+    def __init__(self):
+        self.perms = {}
+
+    ## public methods #########################
+
+    def check(required):
+        pass
+
+    def hasPerm(required):
+        pass
+
+    def perm_sel(): #@TODO: wtf?        
+        pass
+    
+    ## internal methods #######################
+
+    def _permsum(rights):
+        pass
+
+    def _perm_invalid(doeshave, musthave):
+        pass
+
+
+if __name__ == "__main__":
+    pass
