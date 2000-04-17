51c33f102e2a59bf4c1b9cd693745ee834422fae
sabren <sabren>
2000-04-17 21:53:01 +0000

    *** empty log message ***

---
+++ @gone/weblib/User.py
@@ -0,0 +1,23 @@
+"""
+User.py - emulate PHPLIB's user-session support in python.
+
+@TODO: license
+"""
+
+from weblib import Sess
+
+class User(Sess.Sess):
+
+    ## attributes #############################
+    
+    def __init__(self):
+        Sess.__init__(self)
+
+    ## public methods #########################
+    
+    ## internal methods #######################
+
+
+if __name__ == "__main__":
+    pass
+
