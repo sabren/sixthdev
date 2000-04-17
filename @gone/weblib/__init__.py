c7bf59951bac02e8c5a6ce1c59b0a65d8a79512d
sabren <sabren>
2000-04-17 21:53:01 +0000

    *** empty log message ***

---
+++ @gone/weblib/__init__.py
@@ -0,0 +1,26 @@
+#
+# weblib : a library for web stuff
+# 
+#
+
+
+
+
+def uid():
+    """Returns a 32 character, printable, unique string"""
+    import md5, whrandom, string
+    tmp, uid = "", ""
+    
+    # first, just get some random numbers
+    for i in range(64):
+        tmp = tmp + chr(whrandom.randint(0,255))
+
+    # then make a 16-byte md5 digest...
+    tmp = md5.new(tmp).digest()
+
+    # and, since md5 is unprintable,
+    # reformat it in hexidecimal:
+    for i in tmp:
+        uid = uid + string.zfill(hex(ord(i))[2:],2)        
+
+    return uid
