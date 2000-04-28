15c866f41e7d628bd9eef0c54517375744d84d9c
sabren <sabren>
2000-04-28 07:42:37 +0000

    added testResponse

---
+++ @gone/weblib/test/testResponse.py
@@ -0,0 +1,15 @@
+#
+# testResponse.py - unit tests for Response.py
+#
+
+import unittest
+from weblib import response
+
+class ResponseTestCase(unittest.TestCase):
+    
+    def nocheck_printNormal(self):
+        pass
+    
+    def nocheck_printIncercept(self):
+        pass
+
