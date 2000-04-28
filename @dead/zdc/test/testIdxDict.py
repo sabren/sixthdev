ed1f9b32b614f93b41953e1fd415176f4ed2dd40
sabren <sabren>
2000-04-28 10:44:32 +0000

    first version of testIdxDict

---
+++ @dead/zdc/test/testIdxDict.py
@@ -0,0 +1,20 @@
+#
+# testIdxDict.py - test cases for zdc.IdxDict
+
+import unittest
+import IdxDict
+
+
+class IdxDictTestCase(unittest.TestCase):
+
+    def check_IdxDict(self):
+        import IdxDict
+        idx = IdxDict.IdxDict()
+        idx["a"] = 1
+        idx["b"] = 2
+        idx["c"] = 2
+        idx["a"] = 0
+        idx[1] = 1
+        assert idx.keys() == ['a', 'b', 'c'], "keys are wrong"
+        assert idx[0] == 0, "index is wrong"
+
