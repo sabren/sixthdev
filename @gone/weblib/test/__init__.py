900f47b6ff540ff676d9a3dd5f7757e2163e2baa
sabren <sabren>
2000-04-29 12:53:34 +0000

    added testEngine, cleaned up old junk

--- @gone/weblib/test/__init__.py
+++ @gone/weblib/test/__init__.py
@@ -6,6 +6,7 @@ from testResponse import *
 from testSess import *
 from testAuth import *
 from testPerm import *
+from testEngine import *
 #from testUser import *
 
 import MySQLdb
@@ -17,43 +18,6 @@ suites = {
     "response" : unittest.makeSuite(ResponseTestCase, "check_"),
     "sess" : unittest.makeSuite(SessTestCase, "check_"),
     "auth" : unittest.makeSuite(AuthTestCase, "check_"),
+    "engine" : unittest.makeSuite(EngineTestCase, "check_"),
     }
 
-
-def run(full=0):
-    failures, errors = 0, 0
-    modules = ["Sess", "Auth", "Perm", "User", "Request"]
-    if full:
-        modules.append("uid")
-
-    print "* unit tests"
-    import unittest
-    for m in modules:
-
-        # make a new test suite
-        suite = unittest.TestSuite()
-
-        # get the TestCase...
-        exec("import test" + m)
-        testCaseClass = eval("test" + m + "." + m + "TestCase")
-       
-        # now add one test per "check*" method:
-        for t in dir(testCaseClass):
-            if t[:5] == "check":
-                suite.addTest(testCaseClass(t))
-                
-        # finally, run the suite
-        print "** testing " + m
-        result = unittest.TextTestRunner().run(suite)
-        failures = failures + len(result.failures)
-        errors = errors + len(result.errors)
-        print ""
-
-    # summary
-    print "* summary:\n"
-    print failures, "failures,", errors, "errors in", len(modules), "modules.",
-    if failures + errors == 0:
-        print ":)"
-    else:
-        print ":("
-    print ""
