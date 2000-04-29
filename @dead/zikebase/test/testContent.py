b46cf831d0a4fd5c0dcdc7a60a25940ad31819a4
sabren <sabren>
2000-04-29 07:41:17 +0000

    *** empty log message ***

---
+++ @dead/zikebase/test/testContent.py
@@ -0,0 +1,30 @@
+
+import unittest
+import test
+
+from zikebase import Content
+
+## @TODO: first make sure this really is the test database.
+
+class ContentTestCase(unittest.TestCase):
+
+    def setUp(self):
+        cur = test.dbc.cursor()
+        cur.execute("DELETE FROM base_content")
+
+    def check_add(self):
+        cont = Content(test.dbc)
+        cont.title = "a simple test"
+        cont.save()
+        del cont
+
+        cur = test.dbc.cursor()
+        cur.execute("SELECT title FROM base_content WHERE ID=1")
+        row = cur.fetchone()
+
+        assert row is not None, "adding content failed!"
+        assert row[0]=="a simple test", "title wasn't saved!"
+        
+
+    def tearDown(self):
+	pass
