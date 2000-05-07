dccfc4c8a0162bf2eaf12d39dff116aa31caab41
sabren <sabren>
2000-05-07 02:02:48 +0000

    *** empty log message ***

---
+++ @dead/blogdrive/add.py
@@ -0,0 +1,12 @@
+
+import zikebase
+import weblib
+import sqlTest
+
+weblib.auth.check()
+
+cont = zikebase.Content(sqlTest.dbc)
+cont.title = weblib.request.get("title")
+cont.content = weblib.request.get("content")
+cont.save()
+
