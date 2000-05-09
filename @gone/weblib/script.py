858c8be77b8173a1d1c0b51af9e4e6867d4a68fc
sabren <sabren>
2000-05-09 08:19:43 +0000

    better session support in script mode

--- @gone/weblib/script.py
+++ @gone/weblib/script.py
@@ -5,6 +5,8 @@ $Id$
 """
 import weblib
 
+####### special classes that replace Engine ############
+
 class ScriptResponse(weblib.Response):
     """In CGI-mode, we want to insert headers automatically..."""
 
@@ -20,11 +22,28 @@ class ScriptResponse(weblib.Response):
         sys.stdout = self.out
 
 
+class ScriptSess(weblib.Sess):
+
+    def __setitem__(self, name, value):
+
+        """ We can't freeze once at the end, because
+        garbage collection tends to cause all sorts
+        of problems with freeze().. So, the only other
+        alternative seems to be freezing every time
+        we add something to the session... This is
+        just another reason NOT to use CGI mode."""
+        
+        weblib.Sess.__setitem__(self, name, value)
+        self.freeze()
+
+
 ### and now, the herd of singletons ##########################
 
 weblib.request = weblib.Request()
 weblib.response = ScriptResponse()
 
-weblib.sess = weblib.Sess(weblib.pool)
+weblib.sess = ScriptSess(weblib.pool)
+weblib.sess.start()
+
 weblib.auth = weblib.Auth()
 weblib.perm = weblib.Perm()
