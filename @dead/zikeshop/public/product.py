20fbbeb5b530ab2298d2a7e5d324a9eef45ab397
sabren <sabren>
2000-05-16 18:54:04 +0000

    *** empty log message ***

---
+++ @dead/zikeshop/public/product.py
@@ -0,0 +1,17 @@
+#!/usr/home/sabren/bin/python
+
+
+import weblib.script
+import zikeshop
+from zikeshop import dbc
+
+
+prod = zikeshop.Product(dbc, key={'code': weblib.request.get('code')})
+
+print "<h1>Product:%s</h1>" % prod.product
+print "<p>%s</p>" % prod.descLong
+
+print '<A HREF="cart.py?action=add&code=%s">add to cart</a>' % prod.code
+
+print "<hr>"
+print "zikeshop alpha (c)2000 zike interactive, inc"
