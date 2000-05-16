54fb1224bf4af1d65afe462ae685d1dc976b5bc8
sabren <sabren>
2000-05-16 18:48:54 +0000

    Part.py is just documentation for an interface. it may go away...

---
+++ @gone/weblib/Part.py
@@ -0,0 +1,13 @@
+# weblib.Part
+#
+# this is really just an interface.. nothing actually uses it,
+# though you could use it as a stub for sess, response, etc...
+
+
+class Part:
+    def start(self, *kw):
+        pass
+
+    def stop(self, *kw):
+        pass
+
