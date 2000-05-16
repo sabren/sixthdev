d29a64bbec6144a7cfa2e9059299e7b7d797bc6d
sabren <sabren>
2000-05-16 18:54:04 +0000

    *** empty log message ***

---
+++ @dead/zikeshop/public/category.py
@@ -0,0 +1,19 @@
+#!/usr/home/sabren/bin/python
+
+
+import weblib.script
+from zikeshop import dbc
+
+# for now just show a list of products
+
+cur = dbc.cursor()
+cur.execute("select code, product from shop_product order by code")
+
+print "<h1>Products</h1>"
+
+
+for row in cur.fetchall():
+    print '<A href="product.py?code=%s">%s</A><br>' % (row[0], row[1])
+
+print "<hr>"
+print "zikeshop alpha (c)2000 zike interactive, inc"
