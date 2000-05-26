04446a967099d1efb467ac73946de09835bd62d5
sabren <sabren>
2000-05-26 06:39:37 +0000

    added tests for set__xxx and get_xxx

--- @dead/zdc/test/testObject.py
+++ @dead/zdc/test/testObject.py
@@ -5,14 +5,34 @@ import unittest
 import zdc.Object
 
 
+class TestObj(zdc.Object):
+    def __init__(self):
+        pass
+
+
 class ObjectTestCase(unittest.TestCase):
 
     def check_lockDefault(self):
+        tobj = TestObj()
+        assert tobj._isLocked == 0, "oh no! zdc.Object isn't unlocked by default!"
 
-        ## first, create a dummy class
-        class LockObj(zdc.Object):
-            def __init__(self):
-                pass
 
-        lobj = LockObj()
-        assert lobj._isLocked == 0, "oh no! zdc.Object isn't unlocked by default!"
+    def check_getMethod(self):
+        class GetClass(TestObj):
+            def get_xxx(self):
+                return 1
+
+        obj = GetClass()
+        assert obj.xxx == 1, "get_XXXX() methods don't work!"
+
+
+
+    def check_setMethod(self):
+        class SetClass(TestObj):
+            def set_xxx(self, value):
+                self.xyz = value
+
+        obj = SetClass()
+        obj.xxx = "testme!"
+        assert obj.xyz == "testme!", "set_XXX() methods don't work!"
+
