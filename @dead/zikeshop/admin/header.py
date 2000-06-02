7ad5f4fb5965e95dd454071287bb61232365227e
sabren <sabren>
2000-06-02 10:22:46 +0000

    switched order of links at top

--- @dead/zikeshop/admin/header.py
+++ @dead/zikeshop/admin/header.py
@@ -5,8 +5,7 @@ import weblib
 print weblib.trim(
     """
     <table align="center" border="0" cellspacing="5"><tr>
-    <td><a href="adm_product.py">products</a></td>
     <td><a href="adm_node.py">categories</a></td>
-    <td
+    <td><a href="adm_product.py">products</a></td>
     </tr></table><br>
     """)
