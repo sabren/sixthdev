780fe5c1d8aa5fc0eebed012d742eab14a0d59af
sabren <sabren>
2000-06-02 10:23:36 +0000

    moved objectEditor to zikebase

--- @dead/zikeshop/admin/adm_node.py
+++ @dead/zikeshop/admin/adm_node.py
@@ -1,72 +1,73 @@
+"""
+adm_node.py - node administration for zikebase/zikeshop
 
+$Id$
+"""
+
+import zikeshop
 import zikebase
 import weblib
-
-import zikeshop.admin.header
-
 import sys
 
-if weblib.request.get("ID"):
-    node = zikebase.Node(ID=weblib.request["ID"])
-else:
-    node = zikebase.Node()
 
+def showForm(node):
 
-if weblib.request.get("action")=='save':
-    node.name = weblib.request.get("name", '')
-    node.parentID = weblib.request.get("parentID", 0)
-    node.descript = weblib.request.get("descript", '')
-    node.save()
-elif weblib.request.get("action")=='delete':
-    print "<b>", node.name, "deleted.</b><br>"
-    node.delete()
-    node = zikebase.Node()
+    if node.ID:
+        print "<b>this is 'edit' mode</b>"
+    else:
+        print "<b>this is 'add' mode</b>"
+        node.name = ''
+        node.parentID = 0
+        node.descript = ''
 
+    print '<form action="adm_node.py" method="POST">'
+    if node.ID:
+        print '<input type="hidden" name="ID" value="%s">' % node.ID
+    print 'name: <input type="text" name="name" value="%s"><br>' % node.name
 
-if node.ID:
-    print "<b>this is 'edit' mode</b>"
-else:
-    print "<b>this is 'add' mode</b>"
-    node.name = ''
-    node.parentID = 0
-    node.descript = ''
+    print 'subcategory of:'
+    cur = zikeshop.dbc.cursor()
+    if not node.ID:
+        node.ID = 0
+    sql = "SELECT ID, path, ID=%s FROM base_node WHERE ID!=%s ORDER BY path" \
+          % (node.parentID, node.ID)
+    cur.execute(sql)
+    print weblib.selectBox("parentID", cur.fetchall(), blank=0)
+    print '(optional) <br>'
 
-
-print '<form action="adm_node.py" method="POST">'
-if node.ID:
-    print '<input type="hidden" name="ID" value="%s">' % node.ID
-print 'name: <input type="text" name="name" value="%s"><br>' % node.name
-
-
-print 'parent:'
-cur = zikeshop.dbc.cursor()
-sql = "SELECT ID, path, "
-if node.parentID:
-    sql = sql + "ID=%s " % node.parentID
-else:
-    sql = sql + "0=1 "
-sql = sql + "from base_node order by path"
-
-cur.execute(sql)
-print weblib.selectBox("parentID", cur.fetchall(), blank=0)
-
-print 'description:<br>'
-print '<textarea cols="30" rows="5" name="descript">%s</textarea><br>' % node.descript
-print '<input name="action" value="save" type="submit">'
-print '</form>'
-
-
-print '<hr>'
-
-cur = zikeshop.dbc.cursor()
-cur.execute("SELECT ID, path FROM base_node order by path")
-
-print '<b><a href="adm_node.py">add new</a></b><br>'
-
-for row in cur.fetchall():
-    print '<A HREF="adm_node.py?action=delete&ID=%s">[x]</a>' % (row[0],)
-    print '<A HREF="adm_node.py?ID=%s">%s</a><br>' % (row[0], row[1])
+    print 'description:<br>'
+    print '<textarea cols="30" rows="5" name="descript">%s</textarea><br>' % node.descript
+    print '<input name="action" value="save" type="submit">'
+    print '</form>'
     
-print '<hr>'
-print 'zikeshop alpha (c)2000 zike interactive, inc'
+
+
+def showLinks():
+    cur = zikeshop.dbc.cursor()
+    cur.execute("SELECT ID, path FROM base_node order by path")
+    print '<b><a href="adm_node.py">add new</a></b><br>'
+
+    for row in cur.fetchall():
+        print '<A HREF="adm_node.py?action=delete&ID=%s">[x]</a>' % (row[0],)
+        print '<A HREF="adm_node.py?ID=%s">%s</a><br>' % (row[0], row[1])
+        
+    
+
+
+if __name__=="__main__":
+    
+    import header
+
+    if weblib.request.get("ID"):
+        ed = zikebase.ObjectEditor(zikebase.Node, ID=weblib.request["ID"])
+    else:
+        ed = zikebase.ObjectEditor(zikebase.Node)
+    ed.act(weblib.request)
+
+    showForm(ed.object)
+    print '<hr>'
+    showLinks()
+    print '<hr>'
+    print 'zikeshop alpha (c)2000 zike interactive, inc'
+
 
--- @dead/zikeshop/admin/adm_product.py
+++ @dead/zikeshop/admin/adm_product.py
@@ -4,86 +4,11 @@ adm_product.py - product administration for zikeshop
 $Id$
 """
 
+import zikebase
 import zikeshop
 import weblib
 import zdc
 import sys
-
-
-class ObjectEditor:
-    """A class for editing descendents of zdc.Object"""
-
-    ## attributes ########################################
-
-    what   = None # what class?
-    object = None # an instance of the class
-    input  = {}
-
-    ## constructor #######################################
-
-    def __init__(self, what, **args):
-        """ex: objed=ObjectEditor(Person, fName="fred", lName="smith")"""
-        self.what = what
-        self.object = apply(self.newObject, (), args)
-
-
-    ## public methods ####################################
-
-    def newObject(self, **which):
-        """Create a new instance of whatever class we're editing."""
-        
-        ## this next bit is python magic that will
-        ## create a "what" and pass "where" to the
-        ## constructor, assigning the result to self.object
-        ##
-        ## in the example above, objed.object would be
-        ## a Person object representing "fred smith".
-        
-        return apply(self.what, (), which)
-
-
-    def act(self, input):
-        """objed.act(input) #input=a dict with a key called 'action'."""
-
-        self.input = input
-        if input.has_key("action"):
-            method = "act_" + input["action"]
-
-            ## more python magic to call the method:
-            if hasattr(self, method):
-                apply(getattr(self, method), ())
-            else:
-                raise "Don't know how to %s!" % action
-
-        else:
-            pass # do nothing - no action given
-
-
-    def tuplize(self, input):
-        if type(input) != type(()):
-            return (input)
-        else:
-            return input
-
-
-    ## actions ###########################################
-
-    def act_delete(self):
-        self.object.delete()
-        self.object = self.newObject()
-        
-
-    def act_save(self):
-        for field in self.object.getEditableAttrs():
-            if self.input.has_key(field):
-                setattr(self.object, field, self.input[field])
-        for field in self.object.getEditableTuples():
-            print "<h2>DOING %s</h2>" % field
-            if self.input.has_key(field):
-                setattr(self.object, field, self.tuplize(self.input[field]))
-        self.object.save()
-
-
         
 
 def showForm(prod):
@@ -92,7 +17,7 @@ def showForm(prod):
     else:
         print "<b>this is 'add' mode</b>"
         prod.descriptLong = ''
-        prod.product = ''
+        prod.name = ''
         prod.code = ''
 
     print '<form action="adm_product.py" method="POST">'
@@ -100,8 +25,8 @@ def showForm(prod):
         print '<input type="hidden" name="ID" value="%s">' % prod.ID
     print 'code: <input type="text" name="code" value="%s"><br>' \
           % prod.code
-    print 'product: <input type="text" name="product" value="%s"><br>' \
-          % prod.product
+    print 'name: <input type="text" name="name" value="%s"><br>' \
+          % prod.name
 
     print 'categories:<br>'
     cur = zikeshop.dbc.cursor()
@@ -121,10 +46,10 @@ def showForm(prod):
     print '</form>'
 
 
-def showProductLinks():
+def showLinks():
     cur = zikeshop.dbc.cursor()
     cur.execute(
-        "SELECT ID, code, product FROM shop_product order by code")
+        "SELECT ID, code, name FROM shop_product order by code")
 
     print '<b><a href="adm_product.py">add new</a></b><br>'
     for row in cur.fetchall():
@@ -134,18 +59,22 @@ def showProductLinks():
         print row[2], "<br>"
 
 
+
+
 if __name__=="__main__":
-    import zikeshop.admin.header
+    import header
 
     if weblib.request.get("ID"):
-        ed = ObjectEditor(zikeshop.Product, ID=weblib.request["ID"])
+        ed = zikebase.ObjectEditor(zikeshop.Product, ID=weblib.request["ID"])
     else:
-        ed = ObjectEditor(zikeshop.Product)
+        ed = zikebase.ObjectEditor(zikeshop.Product)
 
     ed.act(weblib.request)
 
     showForm(ed.object)
     print '<hr>'
-    showProductLinks()
+    showLinks()
     print '<hr>'
     print 'zikeshop alpha (c)2000 zike interactive, inc'
+
+
