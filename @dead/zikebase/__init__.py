406f16f61761324f439ca6f57735f1b425ffc4f1
sabren <sabren>
2000-06-09 19:15:19 +0000

    added docstring, updated submodules

--- @dead/zikebase/__init__.py
+++ @dead/zikebase/__init__.py
@@ -1,8 +1,13 @@
-## zikebase ########################
+"""
+zikebase: a framework for data-driven websites.
+
+$Id$
+"""
 
 from config import dbc
 from Content import Content
 from Picture import Picture
 from User import User
+from UserAuth import UserAuth
 from Node import Node
 from ObjectEditor import ObjectEditor
