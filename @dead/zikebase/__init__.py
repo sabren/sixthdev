5318cc0d14eba0db023c301ab7d258287481c6cb
sabren <sabren>
2000-06-14 03:01:22 +0000

    snazzy new JIT-importing system

--- @dead/zikebase/__init__.py
+++ @dead/zikebase/__init__.py
@@ -5,9 +5,28 @@ $Id$
 """
 
 from config import dbc
-from Content import Content
-from Picture import Picture
-from User import User
-from UserAuth import UserAuth
-from Node import Node
-from ObjectEditor import ObjectEditor
+
+__objs=(
+    'Content',
+    'Picture',
+    'User',
+    'UserAuth',
+    'ObjectEditor',
+    'Node'
+    )
+
+
+## This is an auto-loading function that makes sure classes only get
+## loaded the first time we request them. this is handy not only for
+## keeping a small footprint, but also because it lets you redefine
+## the classes after zikebase has been loaded.. You might want to do
+## this, for example, in a .weblib.py ...
+
+for __item in __objs:
+    _ = "def %s(*args, **kwargs):"           "\n" \
+        "    import zikebase, %s"            "\n" \
+        "    zikebase.%s = %s.%s"            "\n" \
+        "    return apply(%s.%s, args, kwargs)\n"
+    _ = _ % ((__item,) * 7)
+    exec(_)
+
