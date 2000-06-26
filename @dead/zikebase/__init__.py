8777d04e79fff46c09b10cf4a9e6eccf565c33fa
sabren <sabren>
2000-06-26 17:38:15 +0000

    added load(), more docs

--- @dead/zikebase/__init__.py
+++ @dead/zikebase/__init__.py
@@ -2,6 +2,31 @@
 zikebase: a framework for data-driven websites.
 
 $Id$
+
+This package uses a just-in-time import mechanism that makes sure
+classes only get loaded if we request them. This is handy not only for
+keeping a small footprint, but also because it lets you redefine the
+classes after zikebase has been loaded.. You might want to do this,
+for example, in a .weblib.py ... It should be transparent to the user:
+
+>>> import zikebase
+>>> zikebase.Node
+<function Node at a3c80>
+>>> zikebase.Node()
+<zikebase.Node.Node instance at ccd30>
+
+
+... However, we sometimes want to mess with the class directly before
+instantiating it.. Perhaps to mess with the defaults or whatnot.  The
+load() function lets us do that. For example:
+
+>>> import zikebase
+>>> zikebase.Node
+<function Node at a3c80>
+>>> zikebase.load("Node")
+>>> zikebase.Node
+<class zikebase.Node.Node at f3700>
+
 """
 
 from config import dbc
@@ -15,13 +40,6 @@ __objs=(
     'Node'
     )
 
-
-## This is an auto-loading function that makes sure classes only get
-## loaded the first time we request them. this is handy not only for
-## keeping a small footprint, but also because it lets you redefine
-## the classes after zikebase has been loaded.. You might want to do
-## this, for example, in a .weblib.py ...
-
 for __item in __objs:
     _ = "def %s(*args, **kwargs):"           "\n" \
         "    import zikebase, %s"            "\n" \
@@ -30,3 +48,11 @@ for __item in __objs:
     _ = _ % ((__item,) * 7)
     exec(_)
 
+    
+def load(what):
+    """Loads a class directly, replacing the JIT loader."""
+
+    exec("import zikebase; from %s import %s; zikebase.%s = %s"
+         % ((what,) * 4))
+
+    
