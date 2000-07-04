46318c3577df96973a3a30831d2317f2395bc260
sabren <sabren>
2000-07-04 19:36:53 +0000

    genericized password class

--- @dead/zikebase/User.py
+++ @dead/zikebase/User.py
@@ -5,22 +5,25 @@ $Id$
 """
 
 import zikebase
+zikebase.load("Password")
 import zdc
 
 class User(zdc.RecordObject):
     _table = zdc.Table(zikebase.dbc, "base_user")
     _defaults = {}
 
+    passwordClass = zikebase.Password
+
 
     # we want to encrypt the passwords transparently.
 
     def get_password(self):
         """returns a zikebase.Password object for testing against plaintext."""
-        return zikebase.Password(self.__dict__["cryptedpass"])
+        return self.passwordClass(self.__dict__["cryptedpass"])
     
     def set_password(self, value):
         "user.password = 'whatever'  # Transparently encrypt the password"
-        pw = zikebase.Password()
+        pw = self.passwordClass()
         pw.set(value)
         self.__dict__["cryptedpass"] = pw.crypted
         
