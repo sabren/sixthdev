7bbeb6665444805d654abc8dd58eaf1cef430321
sabren <sabren>
2000-07-22 22:00:53 +0000

    got for, if/el/ef working

--- @gone/zebra/Bootstrap.py
+++ @gone/zebra/Bootstrap.py
@@ -4,15 +4,149 @@ Bootstrap compiler for Zebra.
 $Id$
 """
 import zebra
+import xml2mdl
+
 class Bootstrap:
-    """A class to compile zebra reports until zebra can compile itself."""
+    "A class to compile zebra reports until zebra can compile itself."
+
+    parserClass = xml2mdl.X2mParser
+
+
+    def toObject(self, zbx):
+        "bstrap.toObject(zbx) => a python Report object"
+
+        exec(self.compile(zbx))
+        return Report()
+
+
+    def parse(self, zbx):
+        "Returns a Model-style representation of zbx"
+
+        parser = self.parserClass()
+        parser.feed(zbx)
+        return parser.model
+
 
     def compile(self, zbx):
-        """Bootstrap.().compile(zbx) => python version of zbx"""
-        
-        return zebra.trim(
-            """
-            def fetch(model=None):
-                pass
-            """)
+        "Bootstrap.().compile(zbx) => python code for zbx"
 
+        return self.walk(self.parse(zbx))
+
+
+    def walk(self, model):
+        "Walks along the model, converting it to code..."
+        import types
+
+        res = ""
+
+        for item in model:
+            ## XML tags are represented as dicts
+            if type(item) == types.DictType:
+
+                ## do we have a handler for the tag?
+                assert hasattr(self, "handle_" + item["__tag__"]), \
+                       "Don't know how to handle <%s>" % item["__tag__"]
+
+                res = res + apply(getattr(self, "handle_" + item["__tag__"]),
+                                  (item["__data__"], item))
+            
+
+            ## CDATA is represented as strings
+            elif type(item) == types.StringType:
+
+                ## strip first and last newlines, if present
+                if item and item[0]=="\n": item = item[1:]
+                if item and item[-1]=="\n": item = item[:-1]
+
+                if item:
+                    res = res + "res = res + '%s'\n" \
+                          % zebra.escape(item)
+            
+            else:
+                raise TypeError, \
+                      "Don't know how to cope with %s" % type(item)
+        return res
+
+
+    ## individual tag templates ####################################
+
+    ## <zebra> ##
+    def handle_zebra(self, model, attrs):
+        res = zebra.trim(
+            """
+            class Report:
+            
+                def show(self, model=[]):
+                    print self.fetch(model)
+
+                def fetch(self, model=[]):
+                    self.model = model
+                    res = ""
+            """)
+        res = res + zebra.indent(self.walk(model), 2)
+        res = res + zebra.trim(
+            """
+            # end of fetch()
+                    return res
+            """)
+        return res
+
+
+    ## <for> ##
+    def handle_for(self, model, attrs):
+        res = zebra.trim(
+            """
+            __max__ = len(self.model["%s"])
+            for _ in range(__max__):
+                #@TODO: take this out of the global namespace.
+                #(either figure out why local doesn't work, or
+                #just wrap it all in an exec()
+                globals().update(self.model["%s"][_])
+            """ % (attrs["series"], attrs["series"]))
+        res = res + zebra.indent(self.walk(model), 1)
+
+        # now handle the glue clause:
+        
+        if attrs.has_key("glue"):
+            res = res + zebra.trim(
+            """
+            # glue:
+                if _ + 1 < __max__:
+                    res = res + '%s'
+            """ % (zebra.escape(attrs["glue"])))
+            
+        res = res + zebra.trim(
+            """
+            del _
+            """)
+        return res
+
+
+    ## <var> ##
+    def handle_var(self, model, attrs):
+        res = "res = res + %s\n" % model[0]
+        return res
+
+
+    ## <if> ##
+    def handle_if(self, model, attrs):
+        res = "if %s:\n" % attrs["condition"]
+        res = res + zebra.indent(self.walk(model), 1)
+        return res
+
+    ## <ef> ##
+    def handle_ef(self, model, attrs):
+        res = "elif %s:\n" % attrs["condition"]
+        res = res + zebra.indent(self.walk(model), 1)
+        return res
+
+    ## <el> ##
+    def handle_el(self, model, attrs):
+        res = "else:\n"
+        res = res + zebra.indent(self.walk(model), 1)
+        return res
+
+
+    ## <br> ##
+    def handle_br(self, model, attrs):
+        return 'res = res + "\\n"\n'
--- @gone/zebra/test/testBootstrap.py
+++ @gone/zebra/test/testBootstrap.py
@@ -6,6 +6,28 @@ import zebra
 
 class BootstrapTestCase(unittest.TestCase):
 
+    def check_basics(self):
+        zbx = zebra.trim(
+            """
+            <zebra>
+            hello, world!
+            </zebra>
+            """)
+
+        rpt = zebra.Bootstrap().toObject(zbx)
+
+        for item in ("fetch", "show"):
+            assert hasattr(rpt, item), \
+               "Report objects don't have .%s()!" % item
+
+        actual = rpt.fetch()
+        assert actual == "hello, world!", \
+               "basic 'hello, world' doesn't work:\n%s" % actual
+
+        assert actual == rpt.fetch(), \
+               "calling fetch() a second time yields different results. :/"
+
+
     def check_for(self):
 
         model = {
@@ -20,13 +42,12 @@ class BootstrapTestCase(unittest.TestCase):
             """
             <?xml version="1.0"?>
             <zebra>
-              <for series="stuff">
-              {a}, {b}, {c}
-              
-              </for>
+            <for series="stuff">
+            <var>a</var>, <var>b</var>, <var>c</var>
+            <br/>            
+            </for>
             </zebra>
             """)
-
 
         goal = zebra.trim(
             """
@@ -35,9 +56,39 @@ class BootstrapTestCase(unittest.TestCase):
             alice, betty, carol
             """)
         
-        exec(zebra.Bootstrap().compile(zbx))
-        actual = fetch(model)
 
+        actual = zebra.Bootstrap().toObject(zbx).fetch(model)
         assert actual == goal, \
-               "for() loops don't work."
+               "for() loops don't work:\n---\n%s---" % actual
+
+
+    def check_conditionals(self):
+        model = {
+            "names": [
+            {"name":"a"},
+            {"name":"b"},
+            {"name":"c"}]
+            }
+        
+        zbx = zebra.trim(
+            """
+            <?xml version="1.0"?>
+            <zebra>
+            <for series="names" glue=", ">
+            <if condition="name=='a'">Argentina</if>
+            <ef condition="name=='b'">Bolivia</ef>
+            <el>Chile</el>
+            </for>
+            </zebra>
+            """)
+        
+        goal = "Argentina, Bolivia, Chile"
+
+        #print '--------'
+        #print zebra.Bootstrap().compile(zbx)
+        #print '--------'
+        
+        actual = zebra.Bootstrap().toObject(zbx).fetch(model)
+        assert actual== goal, \
+               "if/el/ef don't work:\n---%s---" % actual
 
