8e2f19ded3322b8f06aba039f0c52f08261a19f9
sabren <sabren>
2000-07-23 06:23:02 +0000

    deleted a bunch of junk I don't need anymore

--- @gone/zebra/__init__.py
+++ @gone/zebra/__init__.py
@@ -4,11 +4,10 @@ Zebra: a text generation and reporting system
 $Id$
 """
 import string
-from Generator import Generator
-from Parser import Parser
-from Engine import Engine
-from Bootstrap import Bootstrap
+import zbr2xml
 import xml2mdl
+from Bootstrap import Bootstrap
+
 
 ###[ Utility Functions ]#######################################
 
@@ -60,41 +59,3 @@ def indent(s, depth=1, indenter="    "):
         lines[i] = (indenter * depth) + lines[i]
 
     return string.join(lines, "\n") + trailer
-
-
-def showdiff(a, b):
-    assert type(a)==type(b), \
-           "can't compare " + `type(a)` + " to " + `type(b)`
-    if type(a)==type([]):
-        a = string.join(a, "\n")
-        b = string.join(b, "\n")
-    return "these don't match:\n[" + a + "]\n--\n[" + b + ']'
-
-
-def stripuid (uidString):
-    """utility for comparing strings with embedded UID's"""
-    def uidrepl(matchobj):
-        return "$$UID$$"
-    return re.sub ('[a-f0-9]{32}', uidrepl, uidString)
-
-
-
-def uid():
-    """Returns a 32 character, printable, unique string"""
-    import md5, whrandom, string
-    tmp, uid = "", ""
-    
-    # first, just get some random numbers
-    for i in range(64):
-        tmp = tmp + chr(whrandom.randint(0,255))
-
-    # then make a 16-byte md5 digest...
-    tmp = md5.new(tmp).digest()
-
-    # and, since md5 is unprintable,
-    # reformat it in hexidecimal:
-    for i in tmp:
-        uid = uid + string.zfill(hex(ord(i))[2:],2)        
-
-    return uid
-
