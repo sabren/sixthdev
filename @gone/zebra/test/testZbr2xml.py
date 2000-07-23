5dfce3c3995a4efa3c3473b53b30dc583e146a28
sabren <sabren>
2000-07-23 21:50:25 +0000

    continued to flesh out z2x

--- @gone/zebra/test/testZbr2xml.py
+++ @gone/zebra/test/testZbr2xml.py
@@ -29,11 +29,17 @@ class Zbr2xmlTestCase(unittest.TestCase):
 
         
     def check_indent(self):
+
+        ## the blank line before the * checks for a
+        ## whitespace error!
         zbr = zebra.trim(
             """
             This is normal text.
-            * if 1=2:
+            
+            * if 1==2:
                 This should never show up.
+            * el:
+            This line isn't part of the else.
             """)
 
         goal = zebra.trim(
@@ -41,12 +47,98 @@ class Zbr2xmlTestCase(unittest.TestCase):
             <?xml version="1.0"?>
             <zebra>
             This is normal text.
-            <if condition="1=2">
+            
+            <if condition="1==2">
             This should never show up.
             </if>
+            <el>
+            </el>
+            This line isn't part of the else.
             </zebra>
             """)
         
         actual = zebra.zbr2xml.Z2X().translate(zbr)
         assert actual==goal, \
                "doesn't indent correctly:\n%s" % actual
+
+
+
+    def check_xmlchars(self):
+        zbr = zebra.trim(
+            """
+            <P>Hello&nbsp;World!!! ><
+            """)
+        goal = zebra.trim(
+            """
+            <?xml version="1.0"?>
+            <zebra>
+            &lt;P&gt;Hello&amp;nbsp;World!!! &gt;&lt;
+            </zebra>
+            """)
+
+        actual = zebra.zbr2xml.Z2X().translate(zbr)
+        assert actual==goal, \
+               "doesn't cope with xmlchars correctly:\n%s" % actual
+
+
+    def check_var(self):
+        zbr = zebra.trim(
+            """
+            My name is {name}.
+            """)
+
+        goal = zebra.trim(
+            """
+            <?xml version="1.0"?>
+            <zebra>
+            My name is <var>name</var>.
+            </zebra>
+            """)
+        actual = zebra.Z2X().translate(zbr)
+        assert actual==goal, \
+               "doesn't cope well with {vars}:\n%s" % actual
+
+
+
+    def check_invalid(self):
+        "check invalid tags"
+        zbr = zebra.trim(
+            """
+            * thisIsAnInvalidTag
+            """)
+        try:
+            zebra.Z2X().translate(zbr)
+        except:
+            gotError = 1
+        else:
+            gotError = 0
+
+        assert gotError, \
+               "Didn't get error on invalid tag."
+
+
+    def check_for(self):
+        zbr = zebra.trim(
+            """
+            * for people:
+                {name} is a nice person.
+            * none:
+                No people here!
+            """)
+        
+        goal = zebra.trim(
+            """
+            <?xml version="1.0"?>
+            <zebra>
+            <for series="people">
+            <var>name</var> is a nice person.
+            </for>
+            <none>
+            No people here!
+            </none>
+            </zebra>
+            """)
+
+        actual = zebra.Z2X().translate(zbr)
+        assert actual==goal, \
+               "Doesn't do for..none right:\n%s" % actual
--- @gone/zebra/zbr2xml.py
+++ @gone/zebra/zbr2xml.py
@@ -44,8 +44,12 @@ class Z2X:
             ## but space in there a little later..
             line = lines[x][left:]
 
+            ## compress blank lines
+            if string.strip(line)=="":
+                res = res + "\n"
+
             ## zebra lines begin with *
-            if line[left] == "*":
+            elif string.lstrip(line)[0] == "*":
                 line = string.strip(line)
 
                 ## .. and end with :
@@ -64,19 +68,32 @@ class Z2X:
                     raise "Don't know how to handle '%s' on line %i" \
                           % (line, x+1)
 
+                ## find the new left edge:
+                topx = x = x + 1
+                for i in range(len(lines[topx])):
+                    if lines[topx][i]!=" ":
+                        newleft = i
+                        break
+
                 ## find the end of the block, which should be
                 ## less indented than the inside of the block.
-                topx = x = x + 1
-                newleft = left + 4
-                while (x<len(lines)) and (lines[x][:newleft])==(" " * 4):
-                    x = x + 1
+                if newleft <= left:
+                    ## the block is empty
+                    pass
+                else:
+                    while (x<len(lines)) \
+                          and (lines[x][:newleft])==(" " * newleft):
+                        x = x + 1
                     
                 ## run this routine recursively on the inner block:
                 res = res + self._deBlock(lines[topx:x], tok, attrs, newleft)
+
+                ## we've already added 1, so jump back to the top:
+                continue
                 
             ## just a normal line..
             else:
-                res = res + line + "\n"
+                res = res + deVar(xmlEncode(line)) + "\n"
 
             ## move on to the next line and continue with the while() loop:
             x = x + 1
@@ -91,5 +108,63 @@ class Z2X:
     def parse_if(self, tokens):
         return 'condition="%s"' % string.join(tokens[1:], " ")
 
+    def parse_ef(self, tokens):
+        return 'condition="%s"' % string.join(tokens[1:], " ")
+
+    def parse_el(self, tokens):
+        return '' # el has no options (yet)
+
     def parse_for(self, tokens):
-        return ""
+        return 'series="%s"' % tokens[1]
+
+    def parse_none(self, tokens):
+        return '' # none has no options
+
+    def parse_head(self, tokens):
+        return '' # no options yet
+
+    def parse_foot(self, tokens):
+        return '' # no options yet
+
+    def parse_glue(self, tokens):
+        return '' # no options yet
+
+
+### HELPER FUNCTIONS ############################################
+
+_entitymap = {
+    "<" : "lt",
+    ">" : "gt" ,   
+    "&" : "amp",      
+    }
+
+def xmlEncode(s):
+    "Converts <, >, and & to xml entities."
+    res = ""
+    for ch in s:
+        if _entitymap.has_key(ch):
+            res = res + "&" + _entitymap[ch] + ";"
+        else:
+            res = res + ch
+    return res
+
+
+def deVar(s):
+    "converts {} to <var> tags. use backslash to escape."
+    res = ""
+    esc = 0
+    for ch in s:
+        if esc:
+            res = res + ch
+            esc = 0
+        elif ch == "\\":
+            esc = 1
+        elif ch=="{":
+            res = res + "<var>"
+        elif ch=="}":
+            res = res + "</var>"
+        else:
+            res = res + ch
+    return res
+
+
