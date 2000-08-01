30e1ec1fed5faf7f550a75255616680a5542e638
sabren <sabren>
2000-08-01 11:48:37 +0000

    added testAuthorizeNetPayment

--- @dead/payment/test/__init__.py
+++ @dead/payment/test/__init__.py
@@ -4,9 +4,11 @@ test cases for python payment module
 __ver__="$Id$"
 
 from testPayment import *
+from testAuthorizeNetPayment import *
 import unittest
 
 suites = {
     "payment" : unittest.makeSuite(PaymentTestCase, "check_"),
+    "authorize.net":unittest.makeSuite(AuthorizeNetPaymentTestCase, "check_"),
     }
 
---
+++ @dead/payment/test/testAuthorizeNetPayment.py
@@ -0,0 +1,10 @@
+"""
+test cases for AuthorizeNetPayment
+"""
+__ver__="$Id$"
+
+import unittest
+import payment
+
+class AuthorizeNetPaymentTestCase(unittest.TestCase):
+    pass
