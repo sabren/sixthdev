a54080abbf65eac768eaf44da36c53d94e6670a0
sabren <sabren>
2000-08-01 11:08:22 +0000

    *** empty log message ***

---
+++ @dead/payment/test/testAccount.py
@@ -0,0 +1,12 @@
+"""
+test cases for main payment routines
+"""
+__ver__="$Id$"
+
+import unittest
+import payment
+
+class AccountTestCase(unittest.TestCase):
+
+    def check_basics(self):
+        pass
