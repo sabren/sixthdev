6d7eee10cbd01166ad620c4e118debc8c4fd5130
sabren <sabren>
2000-09-11 01:47:06 +0000

    added getEditableAttrs

--- @dead/zikebase/User.py
+++ @dead/zikebase/User.py
@@ -17,6 +17,9 @@ class User(zdc.RecordObject):
 
     # we want to encrypt the passwords transparently.
 
+    def getEditableAttrs(self):
+        return zdc.RecordObject.getEditableAttrs(self) + ["password"]
+
     def get_password(self):
         """returns a zikebase.Password object for testing against plaintext."""
         return self.passwordClass(self.__dict__["cryptedpass"])
