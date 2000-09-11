30f149372047a96347f48f6b5a06a2b266acf1db
sabren <sabren>
2000-09-11 01:44:11 +0000

    *** empty log message ***

---
+++ @dead/shipment/UpsShipment.py
@@ -0,0 +1,11 @@
+"""
+module to deal with UPS shipments...
+
+I don't really see any reason to make this a class right now.
+"""
+__ver__="$Id$"
+
+
+
+def fetchRate(fromZip, toZip, rateChart):
+    pass
