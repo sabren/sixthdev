c0181db1fa5608d0049534cee0d15b211ecf9f18
sabren <sabren>
2000-10-12 22:54:08 +0000

    added major flexibility to select(), added radio()

--- @gone/weblib/html.py
+++ @gone/weblib/html.py
@@ -1,69 +1,100 @@
 """
 weblib.html - a utility module to help create html forms.
 """
+__ver__="$Id$"
 
-_isReadOnly_ = 0 # yay! everyone loves global variable!
+import weblib
 
 ##[ HTML form elements ]################################
 
-def textarea(name, value, cols, rows):
+def textarea(name, value, attrs=''):
     '''
     An html TextArea tag
     '''
-    return '<textarea name="%s" cols="%i" rows="%i" ' \
-           'name="comments">%s</textarea><br>' \
-           % (name, cols, rows, weblib.htmlEncode(weblib.deNone(value)))
+    return '<textarea name="%s"%s>%s</textarea>' \
+           % (name, attrs, weblib.htmlEncode(weblib.deNone(value)))
 
 
-def checkbox(name, isChecked):
+def checkbox(name, value, isChecked, attrs=''):
     '''
     An html checkbox. Also adds a hidden __expect__ variable
     since the browser doesn\'t often send unchecked checkboxes.
     '''
     return '<input type="hidden" name="__expect__" value="%s;0">' \
-           '<input type="checkbox" value="1" name="%s"%s>' \
-           % (name, name, ['','CHECKED'][isChecked])
+           '<input type="checkbox" name="%s" %s %s value="%s">' \
+           % (name, name, attrs, ['','CHECKED'][isChecked], value)
 
-def text(name, value, width=None, size=None, maxlength=None,
-         readonly=None, attrs=''):
+def radio(name, value, isChecked, attrs=''):
+    '''
+    An html radio button. 
+    '''
+    return '<input type="radio" name="%s" %s %s value="%s">' \
+           % (name, attrs, ['','CHECKED'][isChecked], value)
+
+def text(name, value, attrs=''):
     '''
     Returns the HTML code for a text INPUT tag.
     '''
-    global _isReadOnly_
-    readonly = _isReadOnly_
-    if readonly:
-        res = value
-    else:
-        #@TODO: should be a global way of turning off None, right?
-        res = '<input type="text" name="%s" value="%s"' \
-              % (name, weblib.deNone(value))
-        if size:
-            res = res + ' size="%i"' % size
-        if maxlength:
-            res = res + ' maxlength="%i"' % size
-        res = res + '>'
-    return res
+    return '<input type="text" name="%s" %s value="%s">' \
+           % (name, attrs, weblib.deNone(value))
 
-def hidden(name, value=None, attrs=''):
+def hidden(name, value, attrs=''):
     '''
     Returns HTML code for a hidden input tag.
     '''
-    return '<input type="hidden" name="%s" value="%s">' \
-           % (name, weblib.deNone(value))
+    return '<input type="hidden" name="%s" %s value="%s">' \
+           % (name, attrs, weblib.deNone(value))
 
 
-def select(name, value, options, attrs=''):
+def select(name, options, value=None, attrs=''):
     '''
     returns HTML for a select box.
-    opttions is a sequence of (key/value) sequences..
+    options is either:
+        a sequence of keys (if keys and values are the same)
+        a sequence of (key/value) sequences..
+    value is either a key or list of keys (can be [])
     attrs is extra HTML to add to the thing..
     '''
-    res = '<select name="%s"%s>' % (name, attrs)
-    for option in  options:
+
+    ## make sure vals is a list
+    if type(value)!=type([]):
+        vals = [value]
+    else:
+        vals = value
+
+    ## expand options into a X*3 grid (if it's not):
+
+    # if options is a sequence of sequences:
+    if type(options[0]) in (type([]), type(())):
+        case = len(options[0])
+        ## if options is a list of 3-tuples:
+        if case == 3:
+            ## leave it as is, ignore values
+            opts = options
+        ## elif options is a list of 2-tuples:
+        elif case == 2:
+            ## loop through and add isChecked
+            opts = []
+            for item in options:
+                opts.append(list(item) + [(item[0] in vals)])
+        else:
+            raise TypeError, \
+                  "Invalid option structure passed to html.select()!"
+    ## else options is a list of keys:
+    else:
+        ## loop through and add make it [key key isChecked]
+        opts = []
+        for item in options:
+            opts.append([item, item, (item in vals)])
+
+    ## now that we have an X*3 grid, show the box:
+    res = '<select name="%s" %s>' % (name, attrs)
+    for option in  opts:
         res = res + '<option value="%s"' % option[0]
-        if option[0] == value:
+        if option[2]:
             res = res +  'SELECTED'
         res = res + '>%s</option>' % option[1]
     return res + '</select>'
+
 
 
