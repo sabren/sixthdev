43586d74fa52f57eaa6476b411729aa21bb3ebe1
sabren <sabren>
2000-11-23 17:25:37 +0000

    minor changes

--- @dead/zikeshop/public/.weblib.py
+++ @dead/zikeshop/public/.weblib.py
@@ -2,11 +2,11 @@
 #print ""
 #import sys; sys.stderr = sys.stdout
 
-import zikeshop
+import zikeshop, weblib
 zikeshop.siteID = 1
 zikeshop.basehref = "/zikeshop/public"
 zikeshop.checkouturl = "checkout.py?auth_logout_flag=1"
-zikeshop.authorizenetmerchant = "aggressive"
+zikeshop.authorizenetmerchant = None #"aggressive"
 zikeshop.owneremail = "terrys_weblib_py@sabren.com" # for cashier.act_checkout
 weblib.auth = zikeshop.CustomerAuth()
 
