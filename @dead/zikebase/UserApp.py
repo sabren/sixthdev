e9265fe1fc592752a46e2328eea4b0e7ddf2d3d3
sabren <sabren>
2000-11-23 17:56:15 +0000

    added auto-login after signup

--- @dead/zikebase/UserApp.py
+++ @dead/zikebase/UserApp.py
@@ -9,7 +9,12 @@ zikebase.load("AppActor")
 class UserApp(zikebase.AppActor):
 
     def act_create(self):
-
         # create the user:
         ed = zikebase.ObjectEditor(zikebase.User, input=self.input)
         ed.do("save")
+
+        # now log in as that user:
+        import weblib
+        if hasattr(weblib, "auth"):
+            weblib.auth.login(ed.object.ID)
+
