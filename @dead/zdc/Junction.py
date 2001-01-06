3e724c5c8cdc5d3469252de54bb89903c6e8ba3d
sabren <sabren>
2001-01-06 18:33:46 +0000

    added a real Junction object, (factored out of zikeshop)

--- @dead/zdc/Junction.py
+++ @dead/zdc/Junction.py
@@ -5,8 +5,8 @@ __ver__="$Id$"
 
 import zdc
 
-class Junction(zdc.LinkSet):
-    __super = zdc.LinkSet
+class Junction(zdc.IdxDict):
+    __super = zdc.IdxDict
     """
     This does the grunt work for working with
     foreign keys in a *:* relationship.
@@ -16,14 +16,59 @@ class Junction(zdc.LinkSet):
     rKey is remote (or right-hand) key fieldname
     lID is the actual value for the local key
     """
-    __super = zdc.LinkSet
 
-    def __init__(self, owner, rClass, table=None, lKey=None, rKey=None):
-        self.__super.__init__(self, owner, rClass, lKey, rKey)
-        self.table = table
+    def __init__(self, owner, rClass, tablename, lKey, rKey):
+        self.__super.__init__(self)
+        self.owner = owner
+        self.rClass = rClass
+        self.tablename = tablename
+        self.lKey=lKey
+        self.rKey=rKey
 
-    def load(self):
-        pass
+    def delete(self):
+        """
+        delete nodes for this product. used internally.
+        """
+        cur = self.owner._table.dbc.cursor()
+        sql =\
+            """
+            DELETE FROM %s
+            WHERE %s=%s
+            """ % (self.tablename, self.lKey, int(self.owner.ID))
+        cur.execute(sql)
 
+    def fetch(self):
+        if self.owner.ID:
+            cur = self.owner._table.dbc.cursor()
+            sql =\
+                """
+                SELECT %s
+                FROM %s 
+                WHERE %s=%s
+                ORDER BY nodeID
+                """ % (self.rKey, self.tablename, self.lKey,
+                       int(self.owner.ID))
+            cur.execute(sql)
+
+            # cur.execute returns a tuple of tuples, eg ((1,), (2,) ...)
+            # we only want the first value (column) from each tuple.
+            for row in cur.fetchall():
+                self << self.rClass(ID=row[0])
+
+    def IDs(self):
+        #@TODO: do I really need .IDs? (maybe for the edit product form?)
+        return tuple(map( lambda robj: robj.ID, self))
+                
     def save(self):
-        pass
+        # handle the nodes:
+        cur = self.owner._table.dbc.cursor()        
+        self.delete()
+        for robj in self:
+            sql =\
+                """
+                INSERT INTO %s (%s, %s)
+                VALUES (%s, %s)
+                """ % (self.tablename, self.lKey, self.rKey,
+                       int(self.owner.ID), robj.ID)
+            cur.execute(sql)
+
---
+++ @dead/zdc/test/testJunction.py
@@ -0,0 +1,14 @@
+"""
+unit tests for zdc.Junction
+"""
+__ver__="$Id$"
+import unittest
+import zdc
+
+class JunctionTestCase(unittest.TestCase):
+
+    #@TODO: put test cases here!
+    # (they're not here because I factored junction out of
+    # zikeshop.Product and zikeshop.Category, and relied on
+    # the unit tests for those objects to do so.)
+    pass
