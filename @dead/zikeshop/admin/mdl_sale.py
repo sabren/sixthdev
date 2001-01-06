a6110ce229e1c2d3d510eda7996fbffb9301f0f4
sabren <sabren>
2001-01-06 20:29:11 +0000

    NASTY kludge to move sql to zdc

--- @dead/zikeshop/admin/mdl_sale.py
+++ @dead/zikeshop/admin/mdl_sale.py
@@ -6,46 +6,50 @@ import weblib
 import zikeshop
 import zdc
 
-cur = zikeshop.dbc.cursor()
 
 dmin = weblib.request.get("dmin", "")
 dmax = weblib.request.get("dmax", "")
-## affiliateID = 0
 includeFilled = weblib.request.get("includeFilled") == "1"
 name = weblib.request.get("name", "")
 isSearch = weblib.request.get("is_search")
 
 
-sql =\
+select =\
     """
     SELECT s.ID, s.tsSold, bc.fname, bc.lname, s.subtotal, s.total 
     FROM shop_sale s
         LEFT JOIN base_contact bc on s.bill_addressID=bc.ID
         LEFT JOIN base_contact sc on s.ship_addressID=sc.ID
-    WHERE 
     """ 
 
 ## build the where clause
-
-if (dmin): sql = sql + " s.tsSold >= '%s' AND " % dmin
-if (dmax): sql = sql + " s.tsSold <= '%s 23:59:59' AND " % dmax
-if (name): sql = sql + " (bc.fname LIKE '%%%s%%' OR " \
+where = "1=1 "
+if (dmin): where = where + " AND s.tsSold >= '%s' AND " % dmin
+if (dmax): where = where + " AND s.tsSold <= '%s 23:59:59' AND " % dmax
+if (name): where = where + " AND (bc.fname LIKE '%%%s%%' OR " \
                  " bc.lname LIKE '%%%s%%' OR " \
                  " sc.fname LIKE '%%%s%%' OR " \
-                 " sc.lname LIKE '%%%s%%') AND " \
+                 " sc.lname LIKE '%%%s%%') " \
                  % (name, name, name, name)
-
 if not includeFilled:
-    sql = sql + "(s.status!='complete') AND "
+    where = where + "AND (s.status!='complete') "
 
-
-# the 1=1 is so we don't have to test for and strip out the last "and.."
-sql = sql + "1=1 ORDER BY s.tsSold ASC"
-
-cur.execute(sql)
+# @TODO: clean this whole query thing up!
+# kids, don't do this at home.
+ick = []
+sales = zdc.find(zikeshop.Sale, where, orderBy="s.tsSold", _select=select)
+for sale in sales:
+    ick.append({
+        "ID": sale.ID,
+        "tsSold": sale.tsSold,
+        "fname" : sale.billAddress.fname,
+        "lname" : sale.billAddress.lname,
+        "subtotal" : sale.subtotal,
+        "total" : sale.total,
+        })
 
 model = {}
-model["sales"] = zdc.toListDict(cur)
+model["sales"] = ick
 model["dmin"] = dmin
 model["dmax"] = dmax
 model["name"] = name
