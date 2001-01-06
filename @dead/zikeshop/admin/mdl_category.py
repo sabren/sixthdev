5a21419f67fae738710f9f9575a008b8d9426097
sabren <sabren>
2001-01-06 19:25:15 +0000

    refactored to use zdc.find()

--- @dead/zikeshop/admin/mdl_category.py
+++ @dead/zikeshop/admin/mdl_category.py
@@ -3,12 +3,8 @@
 #
 
 import zdc, zikeshop
-
-cur = zikeshop.dbc.cursor()
-sql ="SELECT ID, path FROM base_node order by path"
-cur.execute(sql)
-
-model = {
-    "categories" : zdc.toListDict(cur)
-    }
+cats =  zdc.find(zikeshop.Category, orderBy="path")
+for i in range(len(cats)):
+    cats[i] = zdc.ObjectView(cats[i])
+model = {"categories" : cats}
 
