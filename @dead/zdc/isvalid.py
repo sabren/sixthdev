0280def4dc1c1da4ae26e56b4a88e2acae495021
sabren <sabren>
2001-02-06 03:45:15 +0000

    updated isvalid to validate all emails in my mailing list

--- @dead/zdc/isvalid.py
+++ @dead/zdc/isvalid.py
@@ -6,14 +6,15 @@ __ver__="$Id$"
 
 def email(string):
     import re
-    pattern = r'^(\w|\d|_)+(\.[a-zA-Z0-9_]+)*@(\w|\d|_)+(\.[a-zA-Z0-9_]+)+$'
+    pattern = r'^(\w|\d|_|-)+(\.[a-zA-Z0-9_\-]+)*' \
+              r'@(\w|\d|_|-)+(\.[a-zA-Z0-9_\-]+)+$'
     return re.match(pattern, string)
     
 
 
 if __name__=="__main__":
     assert email('sabren@manifestation.com')
-    assert email('michal.sabren@manifestation.com')
+    assert email('michal.sabren@manifest-station.com')
     assert not email('laskdjf..asdf@sadf.com')
     assert not email('asdf@@asdf.asc')
     assert not email('aslkdjf')
