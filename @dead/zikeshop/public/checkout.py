062ecd6bb2eb0bdedba6d95ed5097c95b596be91
sabren <sabren>
2001-03-07 19:25:00 +0000

    removed debug assertion that shouldn't have been there

--- @dead/zikeshop/public/checkout.py
+++ @dead/zikeshop/public/checkout.py
@@ -46,13 +46,13 @@ class CheckoutApp(zikeshop.PublicApp):
     def act_get_billing(self):
         import zebra
         self.consult(self.billData)
+        self.model['shipToBilling']=(self.billData==self.shipData)
         zebra.show('frm_billing', self.model)
 
 
-    def act_get_shipping(self, refresh=1):
-        import zebra, zdc, zikebase
-        if refresh:
-            self.consult(zdc.ObjectView(zikebase.Contact()))
+    def act_get_shipping(self):
+        import zebra
+        self.consult(self.shipData)
         zebra.show('frm_shipping', self.model)
 
 
@@ -70,7 +70,6 @@ class CheckoutApp(zikeshop.PublicApp):
             ('city','city'),
             ('postal','ZIP/postal code')]
 
-
         if context=="bill":
             for item in self.billData.keys():
                 self.billData[item]=self.input.get(item)
@@ -81,14 +80,17 @@ class CheckoutApp(zikeshop.PublicApp):
         for item in required:
             if not self.input.get(item[0],""):
                 errs.append("The '%s' field is required." % item[1])
+
         if (self.input.get("countryCD")=="US") \
         and not self.input.get("stateCD"):
             errs.append("A state is required for US orders")
+
         try:
             ed = zikebase.ObjectEditor(zikebase.Contact, input=self.input)
             ed.do("update")
         except ValueError, valErrs:
             errs.extend(valErrs[0])
+
         if errs:
             self.model["errors"] = map(lambda e: {"error":e}, errs)
             if context=='bill':
@@ -97,35 +99,50 @@ class CheckoutApp(zikeshop.PublicApp):
                 self.next = "get_shipping"
         else:
             if context=='bill':
-                self.data['bill_addressID']=ed.object.ID
+                self.data['shipToBilling'] = int(self.input.get('shipToBilling',0))
                 if self.input.get('shipToBilling'):
-                    self.data['ship_addressID']=self.data['bill_addressID']
+                    self.shipData = self.billData.copy()
                     self.redirect(action='get_card')
                 else:
                     self.redirect(action='get_shipping')
             elif context=='ship':
-                self.data['ship_addressID']=ed.object.ID
                 self.redirect(action='get_card')
 
     def act_add_card(self):
         # Add a new card to the database:
         import zikebase, zebra
+        errs = []
+
+        for item in self.cardData.keys():
+            self.cardData[item]=self.input.get(item)
+
         try:
-            ed = zikebase.ObjectEditor(zikeshop.Card)
-            ed.do("save")
-            # use the card for the transaction:
-            self.data['cardID'] = ed.object.ID
+            #@TODO: REQUIRE input for objectEditor, model for zebra. (reduce errors!)
+            ed = zikebase.ObjectEditor(zikeshop.Card, input=self.input)
+            ed.do("update")
             #@TODO: ought to check expiration date... (prolly in Card.py)
             #@TODO: resolve - cards with secondary billing addresses?
-            self.redirect(action="checkout")
-        except ValueError, errs:
-            self.complain(errs)
-            zebra.show("frm_card", self.model)
+            #self.redirect(action="confirm")
+            self.next = "checkout"
+        except ValueError, valErrs:
+            #@TODO: SERIOUSLY - clean up this error junk.
+            #@TODO: make a central place to configure these error messages.
+            #@TODO: use the same pattern throughout the app [for i18n, etc]
+            errs.extend(valErrs[0])
+
+        if ed.object.isExpired():
+            errs.append("Expired card.")
+            
+        if errs:
+            self.model["errors"] = map(lambda e: {"error":e}, errs)
+            self.next = "get_card"
+        else:
+            self.redirect(action = "checkout")
 
 
-    def act_set_card(self):
-        self.data['cardID'] = int(self.input['cardID'])
-        self.redirect(action = "checkout")
+##     def act_set_card(self):
+##         self.data['cardID'] = int(self.input['cardID'])
+##         self.redirect(action = "checkout")
 
 
     def act_get_card(self):
@@ -144,6 +161,27 @@ class CheckoutApp(zikeshop.PublicApp):
         self.consult(self.cardData)
         zebra.show("frm_card", self.model)
 
+    def act_confirm(self):
+        import zebra, zdc
+        #@TODO: make a .fromDict or .consult for zdc.RecordObjects
+        # for now, we'll assume this is okay, since the classes have
+        # already validated everything in here:
+        bill = zikebase.Contact(); bill._data = self.billData
+        ship = zikebase.Contact(); ship._data = self.shipData
+        card = zikeshop.Card(); card._data = self.cardData
+
+        #@TODO: lots of duplicate logic here.. clean up
+        store = zikeshop.Store()
+        self.model["details"] = self.cart.q_contents()
+        self.model["grandsubtotal"] = self.cart.subtotal()
+        self.model["salestax"] = store.calcSalesTax(ship,self.cart.subtotal())
+
+        #@TODO: zebra ought to have a * with XXX: like for [xxxx]:
+        self.model["billContact"] = [zdc.ObjectView(bill)]
+        self.model["shipContact"] = [zdc.ObjectView(ship)]
+        self.model["card"] = zdc.ObjectView(card)
+
+        zebra.show("frm_confirm", self.model)
 
     def act_show_receipt(self):
         assert self.data.get("saleID"), \
@@ -152,6 +190,8 @@ class CheckoutApp(zikeshop.PublicApp):
         import zebra, zdc
         sale = zikeshop.Sale(ID=self.data['saleID'])
         self.consult(zdc.ObjectView(sale))
+        self.model["billContact"] = [zdc.ObjectView(sale.billAddress)]
+        self.model["shipContact"] = [zdc.ObjectView(sale.shipAddress)]
         zebra.show("dsp_receipt", self.model)
 
         # clear the session info:
@@ -161,13 +201,19 @@ class CheckoutApp(zikeshop.PublicApp):
 
 
     def act_checkout(self):
+        import zikebase
         sale = zikeshop.Sale()
         shop = zikeshop.Store()
 
         #@TODO: update test suite to ensure cardID <> 0 if it shouldn't be.
-        sale.cardID = self.data.get('cardID', 0)
-        sale.bill_addressID = self.data.get('bill_addressID', 0)
-        sale.ship_addressID = self.data.get('ship_addressID', 0)
+        #@TODO: this was just yanked from confirm area..
+        bill = zikebase.Contact(); bill._data = self.billData; bill.userID = 0; bill.save()
+        ship = zikebase.Contact(); ship._data = self.shipData; ship.userID = 0; ship.save()
+        card = zikeshop.Card(); card._data = self.cardData; card.customerID= 0; card.save()
+
+        sale.cardID = card.ID
+        sale.bill_addressID = bill.ID
+        sale.ship_addressID = ship.ID
         
         for item in self.cart.q_contents():
             det = sale.details.new()
@@ -202,7 +248,7 @@ class CheckoutApp(zikeshop.PublicApp):
             model["customer_email"] = sale.billAddress.email
             zikebase.sendmail(zebra.fetch("eml_receipt", model))
             zikebase.sendmail(zebra.fetch("eml_notify",  model))
-
+            
         self.redirect("checkout.py?action=show_receipt")
         
         
