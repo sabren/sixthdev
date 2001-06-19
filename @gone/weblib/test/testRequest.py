0a8425fdd71d81224d7baec073848317e4859998
sabren <sabren>
2001-06-19 00:45:23 +0000

    minor formatting cleanup

--- @gone/weblib/test/testRequest.py
+++ @gone/weblib/test/testRequest.py
@@ -1,8 +1,8 @@
 """
 testRequest.py - unit tests for Request.py
-
-$Id$
 """
+
+__ver__="$Id$"
 
 import unittest
 import weblib
@@ -11,38 +11,48 @@ class RequestTestCase(unittest.TestCase):
     
     def check_engine(self):
         request = weblib.Request()
-        assert request.engine==weblib, "request.engine doesn't default to weblib. :/"
-        assert weblib.request is request, "request doesn't register itself with weblib"
+        assert request.engine==weblib, \
+               "request.engine doesn't default to weblib. :/"
+        assert weblib.request is request, \
+               "request doesn't register itself with weblib"
         
 
     def check_query(self):
         request = weblib.Request(querystring="a=1&b=2&b=3&e=em+cee+squared")
-        assert request.query["a"] == "1", "simple querystring not working"
-        assert request.query["b"] == ("2", "3"), "doesn't tupleize multiple values"
-        assert request.query["e"] == "em cee squared", "query's urldecoding not working"
+        assert request.query["a"] == "1", \
+               "simple querystring not working"
+        assert request.query["b"] == ("2", "3"), \
+               "doesn't tupleize multiple values"
+        assert request.query["e"] == "em cee squared", \
+               "query's urldecoding not working"
 
 
     def check_getitem(self):
         request = weblib.Request(querystring="a=1&aa=2&aa=3&z1=querystring",
-
                                  form={"b":"2", "z1":"form", "z2":"form"},
-
                                  cookie={"c":"3", "z1":"cookie", "z2":"cookie",
                                          "z3":"cookie"},
-                                 
-                                 environ={"e":"mc2", "z1":"environ", "z2":"environ",
+                                 environ={"e":"mc2",
+                                          "z1":"environ", "z2":"environ",
                                           "z3":"environ", "z4":"environ"})
 
-        assert request["a"] == "1", "getitem scews up on querystring"
-        assert request["aa"] == ("2", "3"), "getitem screws up on multiple values"
-        assert request["b"] == "2", "getitem screws up on forms"
-        assert request["c"] == "3", "getitem screws up on cookies"
+        assert request["a"] == "1", \
+               "getitem scews up on querystring"
+        assert request["aa"] == ("2", "3"), \
+               "getitem screws up on multiple values"
+        assert request["b"] == "2", \
+               "getitem screws up on forms"
+        assert request["c"] == "3", \
+               "getitem screws up on cookies"
 
 
         # it should fetch things in this order:
-        assert request["z1"][0] == "querystring", "getitem goes in wrong order (z1)"
-        assert request["z2"][0] == "form", "getitem goes in wrong order (z2)"
-        assert request["z3"] == "cookie", "getitem goes in wrong order (z3)"
+        assert request["z1"][0] == "querystring", \
+               "getitem goes in wrong order (z1)"
+        assert request["z2"][0] == "form", \
+               "getitem goes in wrong order (z2)"
+        assert request["z3"] == "cookie", \
+               "getitem goes in wrong order (z3)"
 
         # environ is not part of the dictionary interface:
         assert (request.get("e") is None) and (request.get("z4") is None), \
@@ -66,23 +76,26 @@ class RequestTestCase(unittest.TestCase):
 
     def check_get(self):
         request = weblib.Request(querystring="a=1")
-
-        assert request.get("a") == "1", "get breaks for valid keys"
-        assert request.get("b") is None, "get breaks for non-present keys"
+        assert request.get("a") == "1", \
+               "get breaks for valid keys"
+        assert request.get("b") is None, \
+               "get breaks for non-present keys"
 
 
 
     def check_environ(self):
         myenv = {"A":"B"}
         eng = weblib.Engine(request=weblib.Request(environ=myenv))
-        assert eng.request.environ["A"] == "B", "request has wrong passed-in environ"
+        assert eng.request.environ["A"] == "B", \
+               "request has wrong passed-in environ"
 
 
 
     def check_encoding(self):
         request = weblib.Request(content="a=<>")
         assert request.form["a"] == "<>", \
-               "doesn't handle <> correctly (got %s)" % repr(request.form["a"])
+               "doesn't handle <> correctly (got %s)" \
+               % repr(request.form["a"])
 
 
     def check_content(self):
@@ -95,20 +108,21 @@ class RequestTestCase(unittest.TestCase):
                "request doesn't store content correctly."
         
         assert request.contentType == "application/x-www-form-urlencoded", \
-               "request.contentType doesn't default to application/x-www-form-urlencoded"
+               "contentType doesn't default to application/...urlencoded"
 
         assert request.contentLength == 7, \
-               "request.contentLength isn't calculated correctly for passed-in content."
+               "contentLength isn't correct for passed-in content."
 
 
 
     def check_contentType(self):
         request=weblib.Request()
         assert request.contentType == "application/x-www-form-urlencoded", \
-               "request.contentType doesn't default to application/x-www-form-urlencoded"
+               "contentType doesn't default to application/...urlencoded"
 
 
-        request=weblib.Request(environ={"CONTENT_TYPE":"text/plain"}, content="")
+        request=weblib.Request(environ={"CONTENT_TYPE":"text/plain"},
+                               content="")
         assert request.contentType == "text/plain", \
                "request.contentType doesn't read contentType from environment"
 
@@ -120,10 +134,9 @@ class RequestTestCase(unittest.TestCase):
 
     def check_multipart(self):
         request = weblib.Request(
-
             contentType=
             "multipart/form-data; boundary=---------------------------7d035c305e4",
-
+            
             content=weblib.trim(
             """
             -----------------------------7d035c305e4
@@ -149,6 +162,7 @@ class RequestTestCase(unittest.TestCase):
             """
             ))
 
+        #@TODO: try MIME-generation module instead of hard-coding the string..
         assert request.form.get("action")=="upload", \
                weblib.trim(
                """
@@ -156,12 +170,15 @@ class RequestTestCase(unittest.TestCase):
                -----------
                ** NOTE **
                this test works when I test manually, but not via
-               the test script.. The bug is in using StringIO
-               (rather than sys.stdin) with the cgi.FieldStorage but
+               the test script.. The bug appears to be in using StringIO
+               (rather than sys.stdin) with the cgi.FieldStorage, or in
+               some invisible characters in the MIME stuff.. 
                I have never been able to track it down... (help?)
                -----------
                """
                % repr(request.form.get("action")))
+
+
 
 
         assert request.form["upfile"].filename == "C:\mimetest.txt", \
