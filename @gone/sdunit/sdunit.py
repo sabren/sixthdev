01aa22140c5cfed4fec71f2114c65bda60be64df
sabren <sabren>
2001-09-30 07:37:34 +0000

    removed references to zike

--- @gone/sdunit/sdunit.py
+++ @gone/sdunit/sdunit.py
@@ -8,17 +8,17 @@ import unittest
 
 ## custom pyunit classes #########################
 
-class ZikeTestResult(unittest._TextTestResult):
+class SixthDayTestResult(unittest._TextTestResult):
     def printHeader(self):
         if self.errors:
             print
 
-class ZikeTestRunner(unittest.TextTestRunner):
+class SixthDayTestRunner(unittest.TextTestRunner):
     def __init__(self):
         unittest.TextTestRunner.__init__(self, sys.stdout)
 
     def run(self, test):
-        result = ZikeTestResult(self.stream)
+        result = SixthDayTestResult(self.stream)
         test(result)
         result.printResult()
         return result
@@ -37,7 +37,7 @@ def wantSpecificTest():
 def runRequestedTest():
     exec "from test.test%s import %sTestCase; TC=%sTestCase" \
          % ((sys.argv[1],) * 3)
-    ZikeTestRunner().run(unittest.makeSuite(TC, "check_"))
+    SixthDayTestRunner().run(unittest.makeSuite(TC, "check_"))
 
 
 ## main ##########################################
@@ -50,12 +50,12 @@ if __name__=="__main__":
         runRequestedTest()
 
     elif hasattr(testmodule, 'suite'):
-        ZikeTestRunner().run(testmodule.suite)
+        SixthDayTestRunner().run(testmodule.suite)
 
     elif hasattr(testmodule, 'suites'):
         for suite in testmodule.suites.keys():
             print "** testing", suite + ":",
-            ZikeTestRunner().run(testmodule.suites[suite])
+            SixthDayTestRunner().run(testmodule.suites[suite])
             print
     else:
         print "no test suite found."
