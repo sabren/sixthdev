9711ed6b1b960f6c40729c6d4e2f8685f260f4fb
sabren <sabren>
2002-01-21 07:51:14 +0000

    *** empty log message ***

---
+++ cvssh/stub.py
@@ -0,0 +1,11 @@
+"""
+This is just a dummy file I was using
+so I didn't have to run py2exe every time
+I made a change to cvssh.py.
+
+(I compiled this with build.bat and set CVS_RSH=stub)
+"""
+import os
+import popen2
+import socket, select
+exec open("cvssh.py")
