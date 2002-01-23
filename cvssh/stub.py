c24657426ef9659cf0fc20020d2aa9e03b87cb81
sabren <sabren>
2002-01-23 13:00:58 +0000

    updated module list

--- cvssh/stub.py
+++ cvssh/stub.py
@@ -6,6 +6,10 @@ I made a change to cvssh.py.
 (I compiled this with build.bat and set CVS_RSH=stub)
 """
 import os
-import popen2
-import socket, select
+import sys
+import socket
+import select
+import time
+import thread
+import getpass
 exec open("cvssh.py")
