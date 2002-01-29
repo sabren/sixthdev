fd5edf0e1cae64346109be9e7c408293a1b22cf5
sholden <sholden>
2002-01-29 00:07:53 +0000

    Use DateTime as base class, rationalize slightly.

--- @dead/zdc/Date.py
+++ @dead/zdc/Date.py
@@ -7,8 +7,9 @@ import calendar
 import copy
 import time
 import zdc
+from DateTime import DateTime
 
-class Date:
+class Date(DateTime):
     """
     A class to represent dates.
     """
@@ -16,33 +17,9 @@ class Date:
         """
         Construct a Date from a string representation.
         """
-        s = datestr
-        if type(s) != type(""):
-            raise TypeError, "usage: Date(string)"
-        if s == "today":
-            s = "%i-%i-%i %i:%i:%i" % time.localtime(time.time())[:6]
-        if " " in s:
-            date, timeofday = s.split(" ")
-        else:
-            date, timetimeofday = s, None
-        # US dates:
-        if date.find("/") > -1:
-            self.m, self.d, self.y = [int(x) for x in date.split("/")]
-        # MySQL dates:
-        else:
-            self.y, self.m, self.d = [int(x) for x in date.split("-")]
+        DateTime.__init__(self, datestr)
+        self.hh = self.mm = self.ss = 0
 
-    def daysInMonth(self):
-        """
-        returns number of days in the month
-        """
-        return calendar.monthrange(self.y, self.m)[1]
-
-    def daysInYear(self):
-        from operator import add
-        return reduce(add,
-                      [calendar.monthrange(self.y, m+1)[1] for m in range(12)])
-        
     def toSQL(self):
         return "%i-%i-%i" % (self.y, self.m, self.d)
 
@@ -52,6 +29,12 @@ class Date:
     def __cmp__(self, other):
         if isinstance(other, Date):
             return cmp([self.y, self.m, self.d], [other.y, other.m, other.d])
+        elif isinstance(other, DateTime):
+            # NOTE: uses arcane DateTime knowledge.
+            # Originally coded as
+            #    return cmp(other, self)*-1
+            return cmp([self.y, self.m, self.d, 0, 0, 0],
+                       [other.y, other.m, other.d, other.hh, other.mm, other.ss])
         elif other is zdc.TIMESTAMP:
             #@TODO: this is sort of a kludge... timestamp should
             # probably just be a sort of datetime singleton...
@@ -59,34 +42,6 @@ class Date:
             return -1 
         else:
             return cmp(self, Date(other))
-
-    def __add__(self, days):
-        """
-        Add a certain number of days, accounting for months, etc..
-        """
-        res = copy.deepcopy(self)
-        res.d += days
-        while res.d > res.daysInMonth():
-            res.d = res.d - res.daysInMonth()
-            res.m += 1
-            if res.m > 12:
-                res.m = 1
-                res.y += 1
-        return res
-    
-    def __sub__(self, days):
-        """
-        same as __add__, but in reverse..
-        """
-        res = copy.deepcopy(self)
-        res.d -= days
-        while res.d < 1:
-            res.m -= 1
-            if res.m < 1:
-                res.m = 12
-                res.y -= 1
-            res.d += res.daysInMonth()
-        return res
 
     def __str__(self):
         return self.toSQL()
