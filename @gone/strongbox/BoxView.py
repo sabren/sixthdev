450dbc6f21b63183e78fc6bd44e510c8d9277db3
sabren <sabren>
2002-03-22 16:53:30 +0000

    moved in from zdc.ObjectView

---
+++ @gone/strongbox/BoxView.py
@@ -0,0 +1,39 @@
+
+class BoxView:
+    """
+    Builds a view (dict/list data structure) from a Box
+    """
+    __ver__="$Id$"
+
+    def __init__(self,object):
+        self.object = object
+
+    def __getitem__(self, name):
+        # this used to have a try..except block, but it made it
+        # very hard to debug!
+        res = getattr(self.object, name)
+        try:
+            hasLen = 1
+            len(res)
+        except:
+            hasLen = 0
+        if (hasLen) and (type(res) != str):
+            lst = []
+            for item in res:
+                lst.append(BoxView(item))
+            return lst
+        else:
+            return res
+
+    def get(self, name, default=None):
+        return getattr(self.object, name, default)
+
+    def keys(self):
+        return self.object.__attrs__.keys()
+        #@TODO: ObjectView.keys() only works with RecordObjects
+        #map(lambda fld: fld.name, self.object._table.fields) \
+        #return self.object.__values__.keys() \
+        #       + self.object._links.keys()
+               # NOTE: i was only doing the tuple thing
+               #       because of zikeshop.Product
+
