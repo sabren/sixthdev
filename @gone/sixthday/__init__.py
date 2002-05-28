5e3c76e78bfa3e7cb5c2f009fdd560a8db93609e
sabren <sabren>
2002-05-28 06:06:41 +0000

    removed ObjectEditor

--- @gone/sixthday/__init__.py
+++ @gone/sixthday/__init__.py
@@ -1,7 +1,6 @@
 from App import App
 from Auth import Auth
 from AdminApp import AdminApp
-from ObjectEditor import ObjectEditor
 from User import User
 from Node import Node
 from SignupApp import SignupApp
