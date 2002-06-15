c28630f507ad6dd3c00d0bf4b98dc26c11086506
sabren <sabren>
2002-06-15 12:33:57 +0000

    added FFT and fake data, but it's buggy

--- openeeg/proto.py
+++ openeeg/proto.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python2.2
 #
+# openEEG software prototype
+# by michal wallace (sabren@manifestation.com)
+#
 # python prototype of a mind-mirror style biofeedback machine
 # Basically, this is just a spectral analysis program.
 #
-# This version only graphs random data, so it's not really
-# much use yet. :)
+# This version graphs fake data, but doesn't seem to be
+# working right. The fakeSession() function explains what I
+# think the graph should show, but it doesn't work out right.
+# can anyone help?
 #
 # $Id$
 
-## dependencies: ###############################################
+## dependencies: #####################################################
 
 try:
     import Numeric, FFT, RandomArray    # http://www.pfdubois.com/numpy/
@@ -21,9 +26,9 @@ except:
 import whrandom
 import time
 
-## support routines ############################################
+## graphic routines ##################################################
 
-def build_gradient():
+def makeGradient():
     """
     Returns a 160*10 Surface showing green-yellow-red gradient.
     """
@@ -41,38 +46,114 @@ def build_gradient():
     return sprite
 
 
-
-def init(winsize):
+def makeWindow(winsize):
     pygame.init()
     pygame.display.set_caption("openEEG prototype")
     return pygame.display.set_mode(winsize, RESIZABLE, 0)
 
 
+def keepLooping():
+    pygame.display.update()
+    for e in pygame.event.get():
+        if (e.type == KEYUP and e.key == K_ESCAPE) \
+        or (e.type == QUIT):
+            return 0
+    return 1
+
+
+
+## data routines #####################################################
+
+def wave(frequency, sampRate=256.0):
+    """
+    Returns a sampled wave at the given frequency and sample rate.
+
+    This routine is generalized from Eric Hagemann's article at:
+    http://www.onlamp.com/pub/a/python/2001/01/31/numerically.html
+    """
+    return Numeric.sin(2 * Numeric.pi
+                         * (frequency/sampRate)
+                         * Numeric.arange(sampRate))
+
+
+def fakeSession():
+    """
+    Creates ten seconds of completely fake data.
+    """
+    pureAlpha = 10                      # alpha is 8-12hz
+    pureBeta  = 20                      # beta is 13-30hz
+    pureTheta = 6                       # theta is 4-8hz
+    pureDelta = 2                       # delta is 0.5-4hz
+
+    sec = [None] * 10                   # make an empty list
+    
+    # when animated, this should move right up the line:
+    sec[0] = wave(pureDelta)
+    sec[1] = wave(pureTheta)
+    sec[2] = wave(pureAlpha)
+    sec[3] = wave(pureBeta)
+
+    # and this should move back down in pairs:
+    sec[4] = wave(pureBeta)  + wave(pureAlpha)
+    sec[5] = wave(pureAlpha) + wave(pureTheta)
+    sec[6] = wave(pureTheta) + wave(pureDelta)
+    sec[7] = wave(pureDelta) + wave(pureBeta)
+
+    # all four at once:
+    sec[8] = wave(pureDelta) + wave(pureTheta) \
+             + wave(pureAlpha) + wave(pureBeta)
+
+    # and then silence:
+    sec[9] = wave(0)
+
+    return Numeric.concatenate(sec)
+
+
+def makeSpectrogram(slice):
+    """
+    Returns a list of length 32, with the FFT of the slice.
+    We seem to need 64 samples to do this.
+    If the sample rate is 256Hz, then we're talking about
+    1/4th of a second's worth of data here.
+    """
+    assert len(slice)==64, "we want 32 bins, so we need 64 samples"
+
+    res = abs(FFT.real_fft(slice))[:-1] # discard 33rd slot (is this okay?)
+    res = Numeric.floor(res) # round off to integers
+    
+    assert len(res)==32, len(res)
+    return res
+    
+
+## main program ######################################################
+
 def main():
-    #@TODO: un-hardcode all these numbers
-    screen = init(winsize=(200, 400))
-    grad = build_gradient()
+    #@TODO: make names for all these magic numbers...
+    
+    screen = makeWindow(winsize=(200, 400))
+    grad = makeGradient()
     
     black = pygame.Surface((160,10))
     black.fill((0,0,0))
 
-    done = 0
-    while not done:
+    session = fakeSession()
+    t = 0
 
-        data = RandomArray.randint(0, 16, (32,))
+    while keepLooping():
+
+        # simulate aquiring data for 1/4th of a second (64 samples):
+        time.sleep(0.25) 
+        data = makeSpectrogram(session[t:t+64])
+        print data
+        t += 64
+        if t >= len(session):
+            t = 0
+        
+        # draw the gradient, then cover part of it up:
         for i in range(32):
-            # draw the gradient, then cover part of it up:
             screen.blit(grad, (20,         20+i*11))
             screen.blit(black,(20+(data[i]*20), 20+i*11))
-            
-        # time.sleep(0.1) # if you want to slow it down
 
-        pygame.display.update()
-        for e in pygame.event.get():
-            if (e.type == KEYUP and e.key == K_ESCAPE) \
-            or (e.type == QUIT):
-                done = 1
-                
 
 if __name__=="__main__":
     main()
