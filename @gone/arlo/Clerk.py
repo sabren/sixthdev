91d65f5dd1d5478be36e1931f2062d7c467cb68b
sabren <sabren>
2002-07-07 06:40:49 +0000

    refactored Clerk and added test for wide tables

--- @gone/arlo/Clerk.py
+++ @gone/arlo/Clerk.py
@@ -1,42 +1,40 @@
-
 from arlo import LinkInjector
 from pytypes import IdxDict
 from strongbox import attr
 
 class ClerkError(Exception): pass
 
-
 class Clerk:
+    """
+    Clerk is an object-relational mapper, responsible
+    for storing strongbox-style objects in storage
+    systems defined with the 'storage' module.
+
+    Constructor is: Clerk(storage) or Clerk(storage, dbmap)
+    where dbmap is a dictionary mapping classes to table names.
+    """
+   
     def __init__(self, storage, dbmap=None):
         self.storage = storage
         self.dbmap = dbmap or {}
 
-    def _unmap_class(self, klass):
-        if klass in self.dbmap:
-            return self.dbmap[klass]
-        else:
-            return klass.__name__
+    ## public interface ##############################################
         
     def store(self, obj):
-        d = {}
-        for item in obj.__attrs__:
-            # the "if" here filters out links and linksets
-            if type(obj.__attrs__[item]) == attr:
-                d[item] = getattr(obj, item)
-        newdata = self.storage.store(self._unmap_class(obj.__class__), **d)
-        obj.update(**newdata)
-        return obj
+        d = self._object_attrs_as_dict(obj)
+        klass = obj.__class__
+        tablename = self._unmap_class(klass)
 
-    def _unmap_link(self, klass, link, name):
-        try:
-            return self.dbmap[link]
-        except KeyError:
-            raise ClerkError, "no mapping found for %s.%s" \
-                  % (klass.__name__, name)
+        # now we update obj because of db-generated values
+        # (such as autonumbers or timestamps)
+        data_from_db = self.storage.store(tablename, **d)
+        relevant_columns = self._attr_columns(klass, data_from_db)
+        obj.update(**relevant_columns)
+        return obj
         
 
     def fetch(self, klass, ID):
-        attrs, othercols = self._fetch_grouped_columns(klass, ID)
+        attrs, othercols = self._fetch_attr_and_other_columns(klass, ID)
         obj = klass(**attrs)
         for name,link in klass.__get_links__():
             fclass, column = self._unmap_link(klass, link, name)
@@ -45,15 +43,6 @@ class Clerk:
                 inj = LinkInjector(obj, name, self, fclass, fID)
         return obj
 
-    def _fetch_grouped_columns(self, klass, ID):
-        attrs, links = {}, {}
-        rec = self.storage.fetch(self._unmap_class(klass), ID)
-        for item in rec.keys():
-            if item in klass.__attrs__:
-                attrs[item]=rec[item]
-            else:
-                links[item]=rec[item]
-        return attrs, links
 
     def fetch_or_new(self, klass, ID):
         if ID:
@@ -77,6 +66,46 @@ class Clerk:
         obj.update(**vals)
         return self.store(obj)
 
+
+    ### private stuff ###############################################
+
+    def _unmap_class(self, klass):
+        if klass in self.dbmap:
+            return self.dbmap[klass]
+        else:
+            return klass.__name__
+
+    def _object_attrs_as_dict(self, obj):
+        #@TODO: should this be on all strongboxen?
+        d = {}
+        for attrName, attrObject in obj.__attrs__.items():
+            # we only want attrs, not links or linksets:
+            if type(attrObject) == attr:
+                d[attrName] = getattr(obj, attrName)
+        return d
+        
+    def _unmap_link(self, klass, link, name):
+        try:
+            return self.dbmap[link]
+        except KeyError:
+            raise ClerkError, "no mapping found for %s.%s" \
+                  % (klass.__name__, name)
+
+    def _attr_columns(self, klass, rec):
+        return self._attr_and_other_columns(klass, rec)[0]
+
+    def _attr_and_other_columns(self, klass, rec):
+        attrs, others = {}, {}
+        for item in rec.keys():
+            if item in klass.__attrs__:
+                attrs[item]=rec[item]
+            else:
+                others[item]=rec[item]
+        return attrs, others
+
+    def _fetch_attr_and_other_columns(self, klass, ID):
+        rec = self.storage.fetch(self._unmap_class(klass), ID)
+        return self._attr_and_other_columns(klass, rec)
 
 
 
--- @gone/arlo/spec/ClerkTest.py
+++ @gone/arlo/spec/ClerkTest.py
@@ -28,12 +28,18 @@ class ClerkTest(unittest.TestCase):
         actual =self.storage.match("Record")
         assert actual == [{"ID":1, "val":""}]
 
+    def check_store_again(self):
+        self.clerk.store(Record())
+        r = self.clerk.fetch(Record, 1)
+        r.val = "abc"
+        self.clerk.store(r)
         
     def check_fetch(self):
         self.clerk.store(Record(val="howdy"))
         obj = self.clerk.fetch(Record, 1)
         assert obj.val == "howdy"
-        
+
+
     def check_delete(self):
         self.check_fetch()
         self.clerk.delete(Record, 1)
@@ -52,3 +58,33 @@ class ClerkTest(unittest.TestCase):
         assert a.next.next.val == "c"
         assert a.next.next.next is None
 
+
+    def check_fetch_from_wide_table(self):
+        """
+        Supose a strongbox has 1 slot, but the table has 2+ columns.
+        We can't just jam those columns into the strongbox,
+        because strongbox is *designed* to blow up if you try
+        to add new attributes.
+
+        But on the other hand, a DBA should be able to add columns
+        to the databaes without breaking the code and causing
+        AttributeErrors all over the place.
+
+        Instead, Clerk should only use the columns that have
+        matching attributes, and simply ignore the others.
+
+        This sorta violates the concept of OnceAndOnlyOnce,
+        because now the tables can be out of sync with the
+        data model, but I think it's better than the alternative,
+        and this is the sort of thing one could check with
+        an automated tool.
+
+        #@TODO: write tool to compare DB and object models :)
+        """
+        try:
+            self.storage.store("Record", val="a", extra_column="EEK!")
+            a = self.clerk.fetch(Record, 1)
+            a.val="aa"
+            self.clerk.store(a)
+        except AttributeError:
+            self.fail("shouldn't die when columns outnumber attributes")
