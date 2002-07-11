c843cc8a62a4804ff5f13894cf4c8943e13e1449
sabren <sabren>
2002-07-11 21:19:53 +0000

    test case for quotes

--- @gone/storage/spec/MySQLStorageTest.py
+++ @gone/storage/spec/MySQLStorageTest.py
@@ -17,3 +17,18 @@ class MySQLStorageTest(MockStorageTest):
                     name varchar(32)
                 )
                 """)
+
+
+    def check_store_quotes(self):
+        self.populate()
+        row = self.s.fetch("test_person", 1)
+        row["name"] = "j'mo\"cha's'ha''ha"
+        self.s.store("test_person", **row)
+        assert self.wholedb() == [{"ID":1, "name":"j'mo\"cha's'ha''ha"},
+                                  {"ID":2, "name":"wanda"}]        
+        
+
+
+
+
+    # other test are inherited from MockStorage...
