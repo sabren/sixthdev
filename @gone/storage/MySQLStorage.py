e1f010525ffe41eca60e671bf21a274540b57770
sabren <sabren>
2002-07-11 21:19:18 +0000

    fixes to clean up quotes and empty dates

--- @gone/storage/MySQLStorage.py
+++ @gone/storage/MySQLStorage.py
@@ -1,4 +1,5 @@
 from storage import Storage
+from pytypes import Date
 
 class MySQLStorage(Storage):
 
@@ -18,6 +19,16 @@ class MySQLStorage(Storage):
         return res
 
 
+    def _toSQLString(self, val):
+        """
+        Turns a value into a quoted string suitable for MySQL
+        """
+        if isinstance(val, Date) and str(val)=='0-0-0':                
+            return "NULL"
+        else:
+            return "'" + "''".join(str(val).split("'")) + "'"
+            
+
     def _insert(self, table, **row):
 
         # generate column/value lists for INSERT
@@ -25,7 +36,7 @@ class MySQLStorage(Storage):
         for col,val in row.iteritems():
             cols += col + ","
             # ok for numbers in mysql
-            vals += "'" + str(val) + "',"
+            vals += self._toSQLString(val) + ","
 
         # strip off last commas:
         cols = cols[:-1]
@@ -43,7 +54,7 @@ class MySQLStorage(Storage):
 
         sql = "UPDATE " + table + " SET"
         for col,val in row.iteritems():
-            sql += " " + col + "='" + str(val) + "',"
+            sql += " " + col + "=" + self._toSQLString(val) + ","
         sql = sql[:-1]
 
         # whoops! thanks to Andy Todd for this line:
