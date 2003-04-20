f4615d6ac9aa6e5f77ac11cb3d0c2377344da5d4
sabren <sabren>
2003-04-20 08:44:33 +0000

    fixed a test I'd intentionally broken

--- @dead/tourney/tourney.scm
+++ @dead/tourney/tourney.scm
@@ -1,4 +1,6 @@
 
+;$Id$
+
 ;; couldn't get schemeunit working:
 (define (assert condition)
   (if (not (eval condition))
@@ -13,7 +15,7 @@
 (define (tournament-tests)
   (assert '(= 5 (winner max '(1 2 3 4 5))))
   (assert '(= 4 (winner max '(4 3 2 1 0))))
-  (assert '(= 55 (winner max '(1 3 5 2 4))))
+  (assert '(= 5 (winner max '(1 3 5 2 4))))
   (assert '(= 1 (winner min '(1 3 5 2 4)))))
 
 (tournament-tests)
