bb53e9cd5ca03b7b9d36056669bb6d129933933e
mario <mario>
2003-07-13 14:33:46 +0000

    major refactoring

--- ransacker/vectorspace/search_mod.py
+++ ransacker/vectorspace/search_mod.py
@@ -11,189 +11,151 @@ import re
 from string import lower
 from stemmer import PorterStemmer
 from vecmath import vcos
-
-class VectorSpace:
-
-	def __init__(self, docs, stoplist):
-
-		self.word_index = {}
-		self.word_count = 0
-		self.vecs = []
-		self.doc_vecs = []
-		self.threshold = 0.04
-		self.docs = docs
-		self.stop_list = stoplist
-		self.all_words = {}
+import ransacker
 
 
-	def build_index(self):
-		for doc in self.docs:
-		# for doc in self.docs:
-                    self.addDocumentWords(doc)
-                self.mapWordsToPosition()
+class VectorSearchEngine(object):
+    def __init__(self, index):
+        self.index = index
+        word_index = self.mapWordsToPosition()
+        self.vecs = []
+        self.doc_vecs = []
+        self.threshold = 0.04
+        self.word_index = word_index
+        self.build_vectors()
+
+    def build_vectors(self):
+        assert 0, "for next time: get rid of reliance on self.index.docs"
+        for doc in self.index.docs:
+            vec = self.make_vector(doc)
+            # the perl used at this point is
+            #   push @vecs, norm $vec;
+            # must impliment using Numeric
+            self.vecs.append(vec)
+
+        self.doc_vecs.extend(self.vecs)
 
 
-        def build_vectors(self):
-                assert self.word_count > 0, "must call build_index() first!!"
-		for filename in self.docs:
-			vec = self.make_vector(filename)
-			# the perl used at this point is
-			#   push @vecs, norm $vec;
-			# must impliment using Numeric
-			self.vecs.append(vec)
+    def make_vector(self,in_words):
+        # words should be a dictionary
+        words = get_words(in_words)
+        word_count = len(self.word_index.keys())
+        vector = zeros(word_count)
 
-		self.doc_vecs.extend(self.vecs)
+        for w in words.keys():
+            if self.word_index.has_key(w):
+                value = words[w]
+                offset = self.word_index.get(w,0) 
+                vector[offset] = value
+        return vector       
 
+    def search(self,search_for):
+        qvec = self.make_vector(search_for)
+        result_list = self.get_cosines(qvec)
+        documents = {}
+        for index in result_list.keys():
+            doc = self.index.docs[index]
+            relevance = result_list[index]
+            documents[doc] = relevance
+        return documents
 
+    def get_cosines(self,in_qvec):
+        cosines = {}
+        index = 0
 
-	def search(self,search_for):
-		
-		qvec = self.make_vector(search_for)
-		# print "\nqvec says  ", qvec
+        for this_vec in self.doc_vecs:
+            assert isinstance(this_vec, ArrayType)
+            cosine = vcos(this_vec, in_qvec)
+            if cosine > self.threshold:
+                cosines[index] = cosine
+            index = index + 1
+        return cosines
 
-		# result_list is a dictionary
-		# FIX-ME
-		result_list = self.get_cosines(qvec)
-		# print "\nresult_list  ", result_list
-		
-		documents = {}
+    def mapWordsToPosition(self):
+        # create a lookup hash of word to position
+        # originally looked like this
+        # my %lookup;
+        # my @sorted_words = sort keys %all_words;
+        # @lookup{@sorted_words} = (1..$#sorted_words );    
+        lookup = {}
+        x = 0
+        for one in self.index.getWordList():
+            lookup[one] = x
+            x = x + 1
+        ####  TODO: the above section could(and should) be optimized
 
-		for index in result_list.keys():
-			doc = self.docs[index]
-			relevance = result_list[index]
-			documents[doc] = relevance
+        return lookup
 
-		return documents
+class VectorSpace(ransacker.Index):
 
-	def get_words(self,text):
-		# Splits on whitespace and strips some punctuation		
-		# my ( $self, $text ) = @_;
-		# my %doc_words;  
-		# my @words = map { stem($_) }
-		#			grep { !( exists $self->{'stop_list'}->{$_} ) }
-		#			map { lc($_) } 
-		#			map {  $_ =~/([a-z\-']+)/i} 
-		#			split /\s+/, $text;
-		doc_words = {}
-		words = [self.stem(word) for word in text.lower().split()
-			    if re.match("[a-z\-']+", word)
-			    and word not in self.stop_list]
-		
-		# do { $_++ } for @doc_words{@words};
-		for one in words:
-			doc_words.setdefault(one,0)
-			doc_words[one] = doc_words[one] + 1
-			
-		# return %doc_words;
-		return doc_words
+    def __init__(self, docs):
 
-	def stem(self, word):
-		# word needs to be all lowercase before being passed to stem
-		string.lower(word)	
-
-		# fancy stuff to remove .,?!"
-		mymatch = re.compile('(\,|\.|\!|\?|\")')
-		word = mymatch.sub(r'',word)
-
-		p = PorterStemmer()
-		word = p.stem(word, 0,len(word)-1)
-		return word
+        self.docs = docs
+        self.all_words = {}
 
 
-        def addDocumentWords(self, doc):
-            words = self.get_words(doc)            
-            for k in words.keys():
-	        self.all_words.setdefault(k,0)
-		self.all_words[k] += words[k]	
-
-        def mapWordsToPosition(self):
-		# create a lookup hash of word to position
-		# originally looked like this
-		# my %lookup;
-		# my @sorted_words = sort keys %all_words;
-		# @lookup{@sorted_words} = (1..$#sorted_words );	
-		lookup = {}
-		sorted_words = self.all_words.keys()
-		sorted_words.sort()
-		x = 0
-		for one in sorted_words:
-			lookup[one] = x
-			x = x + 1
-		####  TODO: the above section could(and should) be optimized
-
-                self.word_index = lookup
-                self.word_list = sorted_words
-                self.word_count = len(sorted_words)
+    def build_index(self):
+        for doc in self.docs:
+            self.addDocumentWords(doc)
 
 
-	def make_vector(self,in_words):
-		# words should be a dictionary
-		words = self.get_words(in_words)
-		# the perl used at this point is
-		#  my $vector = zeroes $self->{'word_count'}; 
-		vector = zeros(self.word_count)
-                #assert self.word_count > 0
-                #assert type(vector)==array, vector
-		# print "\nthe first vector ", vector
-		
-		# print "\nthe keys are ", words.keys()
-		for w in words.keys():
-			if self.word_index.has_key(w):
-				# print "\nw is ", w
-				# print "\nword_index says  ", word_index
-				value = words[w]
-				# print "\nvalue is", value
-				offset = self.word_index.get(w,0) # <-- turns moose into cats
-				# print "\noffset is ", offset
-				# print "\nvector says ", vector
-				vector[offset] = value
-				# print "\nvector now says ", vector
-                #assert type(vector)==array
-		return vector		
+    def getEngine(self):       
+        return VectorSearchEngine(self)
 
 
-	def get_cosines(self,in_qvec):
-	
-		cosines = {}
-		index = 0
 
-		for this_vec in self.doc_vecs:
-                        assert isinstance(this_vec, ArrayType)
-			cosine = vcos(this_vec, in_qvec)
-			# print "\nnew cosine  ", cosine
-			if cosine > self.threshold:
-				cosines[index] = cosine
+    def addDocumentWords(self, doc):
+        words = get_words(doc)            
+        for k in words.keys():
+            self.all_words.setdefault(k,0)
+            self.all_words[k] += words[k]   
 
-			index = index + 1
+    def getWordList(self):
+        words = self.all_words.keys()
+        words.sort()
+        return words
 
-		return cosines
+
+def get_words(text):
+    stop_list = load_stop_list() # love those ()
+    # Splits on whitespace and strips some punctuation      
+    words = [stem(word) for word in text.lower().split()
+        if re.match("[a-z\-']+", word)
+        and word not in stop_list]
+        
+    # do { $_++ } for @doc_words{@words};
+    doc_words = {}
+    for one in words:
+        doc_words.setdefault(one,0)
+        doc_words[one] = doc_words[one] + 1
+            
+        # return %doc_words;
+    return doc_words
+
+def stem(word):
+    # word needs to be all lowercase before being passed to stem
+    string.lower(word)  
+
+    # fancy stuff to remove .,?!"
+    mymatch = re.compile('(\,|\.|\!|\?|\")')
+    word = mymatch.sub(r'',word)
+
+    p = PorterStemmer()
+    word = p.stem(word, 0,len(word)-1)
+   
+    return word
 
 
 def load_stop_list():
-	all_from_file = []
-	for line in open('stop_list.txt').readlines():
-		mymatch = re.compile('\n')                
-		all_from_file.append(mymatch.sub(r'',line))
-	
-	return all_from_file
-
-
-		
-if __name__=="__main__":
+    all_from_file = []
+    # FIX-ME that path needs tweaking later
+    for line in open('/home/mario/dev/lib/ransacker/vectorspace/stop_list.txt').readlines():
+        mymatch = re.compile('\n')                
+        all_from_file.append(mymatch.sub(r'',line))
     
-    docs = ["dog", "cat", "dog? cat", "cat cat"]
-    engine = VectorSpace(docs, load_stop_list())
-    engine.build_index()
-    assert engine.word_count > 0
-    engine.build_vectors()
+    return all_from_file
 
+        
+if __name__=="__main__":
+    pass
 
-    results = engine.search("dog")
-    assert results.has_key("dog")
-    assert results.has_key("dog? cat")
-    assert len(results.keys()) == 2
-    assert results["dog"] > results["dog? cat"]
-
-    print "it works!"
-
-		
