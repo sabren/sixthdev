fd7581935c83e1eb7cdefc1b1429247f5f71d8ce
sabren <sabren>
2003-07-19 23:20:26 +0000

    cleaned up tests

--- @gone/weblib/spec/RequestTest.py
+++ @gone/weblib/spec/RequestTest.py
@@ -6,133 +6,119 @@ __ver__="$Id$"
 
 import unittest
 import weblib
+from weblib import Request
 
 class RequestTest(unittest.TestCase):
-    
-    def test_engine(self):
-        request = weblib.Request()
-        assert not hasattr(request, "engine")
-        assert not hasattr(weblib, "request")
-        
 
     def test_query(self):
-        request = weblib.Request(querystring="a=1&b=2&b=3&e=em+cee+squared")
-        assert request.query["a"] == "1", \
+        req = Request(querystring="a=1&b=2&b=3&e=em+cee+squared")
+        assert req.query["a"] == "1", \
                "simple querystring not working"
-        assert request.query["b"] == ("2", "3"), \
+        assert req.query["b"] == ("2", "3"), \
                "doesn't tupleize multiple values"
-        assert request.query["e"] == "em cee squared", \
+        assert req.query["e"] == "em cee squared", \
                "query's urldecoding not working"
 
 
     def test_getitem(self):
-        request = weblib.Request(querystring="a=1&aa=2&aa=3&z1=querystring",
-                                 form={"b":"2", "z1":"form", "z2":"form"},
-                                 cookie={"c":"3", "z1":"cookie", "z2":"cookie",
-                                         "z3":"cookie"},
-                                 environ={"e":"mc2",
-                                          "z1":"environ", "z2":"environ",
-                                          "z3":"environ", "z4":"environ"})
-
-        assert request["a"] == "1", \
+        req = Request(querystring="a=1&aa=2&aa=3&z1=querystring",
+                      form={"b":"2", "z1":"form", "z2":"form"},
+                      cookie={"c":"3", "z1":"cookie", "z2":"cookie",
+                              "z3":"cookie"},
+                      environ={"e":"mc2",
+                               "z1":"environ", "z2":"environ",
+                               "z3":"environ", "z4":"environ"})
+        
+        assert req["a"] == "1", \
                "getitem scews up on querystring"
-        assert request["aa"] == ("2", "3"), \
+        assert req["aa"] == ("2", "3"), \
                "getitem screws up on multiple values"
-        assert request["b"] == "2", \
+        assert req["b"] == "2", \
                "getitem screws up on forms"
-        assert request["c"] == "3", \
+        assert req["c"] == "3", \
                "getitem screws up on cookies"
 
-
         # it should fetch things in this order:
-        assert request["z1"][0] == "querystring", \
+        assert req["z1"][0] == "querystring", \
                "getitem goes in wrong order (z1)"
-        assert request["z2"][0] == "form", \
+        assert req["z2"][0] == "form", \
                "getitem goes in wrong order (z2)"
-        assert request["z3"] == "cookie", \
+        assert req["z3"] == "cookie", \
                "getitem goes in wrong order (z3)"
-
+        
         # environ is not part of the dictionary interface:
-        assert (request.get("e") is None) and (request.get("z4") is None), \
+        assert (req.get("e") is None) and (req.get("z4") is None), \
                "request.__getitem__ should not access environ."
 
 
-
     def test_keys(self):
-        req = weblib.Request(querystring="querystring=1",
-                             form={"form":"1"},
-                             cookie={"cookie":"1"},
-                             environ={"environ":"1"})
+        req = Request(querystring="querystring=1",
+                      form={"form":"1"},
+                      cookie={"cookie":"1"},
+                      environ={"environ":"1"})
         keys = req.keys()
         keys.sort()
-
+        
         assert keys== ['cookie','form','querystring'], \
                "request.keys() doesn't work."
 
 
-
-
     def test_get(self):
-        request = weblib.Request(querystring="a=1")
-        assert request.get("a") == "1", \
+        req = Request(querystring="a=1")
+        assert req.get("a") == "1", \
                "get breaks for valid keys"
-        assert request.get("b") is None, \
+        assert req.get("b") is None, \
                "get breaks for non-present keys"
-
 
 
     def test_environ(self):
         myenv = {"A":"B"}
-        eng = weblib.Engine(request=weblib.Request(environ=myenv))
+        eng = weblib.Engine(request=Request(environ=myenv))
         assert eng.request.environ["A"] == "B", \
                "request has wrong passed-in environ"
 
 
-
     def test_encoding(self):
-        request = weblib.Request(content="a=<>")
-        assert request.form["a"] == "<>", \
+        req = Request(content="a=<>")
+        assert req.form["a"] == "<>", \
                "doesn't handle <> correctly (got %s)" \
-               % repr(request.form["a"])
+               % repr(req.form["a"])
 
 
     def test_content(self):
-        request = weblib.Request()
-        assert request.content =="", \
+        req = Request()
+        assert req.content =="", \
                "request.content doesnt default to blank string"
         
-        request = weblib.Request(content="abcdefg")
-        assert request.content == "abcdefg", \
+        req = Request(content="abcdefg")
+        assert req.content == "abcdefg", \
                "request doesn't store content correctly."
         
-        assert request.contentType == "application/x-www-form-urlencoded", \
+        assert req.contentType == "application/x-www-form-urlencoded", \
                "contentType doesn't default to application/...urlencoded"
-
-        assert request.contentLength == 7, \
+        
+        assert req.contentLength == 7, \
                "contentLength isn't correct for passed-in content."
-
+        
 
 
     def test_contentType(self):
-        request=weblib.Request()
-        assert request.contentType == "application/x-www-form-urlencoded", \
+        req=Request()
+        assert req.contentType == "application/x-www-form-urlencoded", \
                "contentType doesn't default to application/...urlencoded"
 
+        req=Request(environ={"CONTENT_TYPE":"text/plain"},
+                        content="")
+        assert req.contentType == "text/plain", \
+               "req.contentType doesn't read contentType from environment"
 
-        request=weblib.Request(environ={"CONTENT_TYPE":"text/plain"},
-                               content="")
-        assert request.contentType == "text/plain", \
-               "request.contentType doesn't read contentType from environment"
-
-        request=weblib.Request(contentType="text/xml")
-        assert request.contentType=="text/xml", \
-               "request.contentType didn't get set based on init's parameter"
-    
-
+        req=Request(contentType="text/xml")
+        assert req.contentType=="text/xml", \
+               "req.contentType didn't get set based on init's parameter"
 
     def test_multipart(self):
         raise "skip"
-        request = weblib.Request(
+        req = Request(
             method="POST",
             contentType=
             "multipart/form-data; boundary=---------------------------7d035c305e4",
@@ -177,17 +163,11 @@ class RequestTest(unittest.TestCase):
                """
                % repr(request.form.get("action")))
 
-
-
-
         assert request.form["upfile"].filename == "C:\mimetest.txt", \
                "file uploads don't return FieldStorage objects"
 
-
         assert request.form["twovalues"] == ("value1", "value2"), \
                "multi-value fields break on multi-part forms."
-
-
 
 
     def test_ampersand(self):
@@ -198,12 +178,12 @@ class RequestTest(unittest.TestCase):
         two pieces...
         """
         ampstr = "a=apple&b=boys%26girls"
-        req = weblib.Request(content=ampstr, querystring=ampstr)
+        req = Request(content=ampstr, querystring=ampstr)
         goal = {"a":"apple", "b":"boys&girls"}
 
         assert req.query == goal, \
-               "Request.query doesn't grok ampersands."
+               ".query doesn't grok ampersands."
 
         assert req.form == goal, \
-               "Request.query doesn't grok ampersands."
+               ".query doesn't grok ampersands."
 
