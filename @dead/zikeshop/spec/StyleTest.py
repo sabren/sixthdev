4bd36def9490c644fb6ade7d08bee5fcbf2e4253
sabren <sabren>
2003-08-24 22:59:44 +0000

    removed useless test

--- @dead/zikeshop/spec/StyleTest.py
+++ @dead/zikeshop/spec/StyleTest.py
@@ -13,9 +13,4 @@ clerk = MockClerk()
 #@TODO: test picture attribute
 
 class StyleTest(unittest.TestCase):
-
-    def test_product(self):
-        #@TODO: what's up with this test?
-        # maybe it just tries not to get an error?
-        style = clerk.fetch(Style, ID=2)
-        prod = style.product
+    pass
