43ee303e0bf6cc6498c518b344f59252fbd119d7
sabren <sabren>
2003-11-23 04:40:38 +0000

    *** empty log message ***

---
+++ boxspring/boxspring.rsrc.py
@@ -0,0 +1,37 @@
+{'stack':{'type':'Stack',
+          'name':'Template',
+    'backgrounds': [
+    {'type':'Background',
+          'name':'bgTemplate',
+          'title':'BoxSpring',
+          'position':(37, 206),
+          'size':(526, 449),
+          'style':['resizeable'],
+
+        'menubar': {'type':'MenuBar',
+         'menus': [
+             {'type':'Menu',
+             'name':'menuFile',
+             'label':'&File',
+             'items': [
+                  {'type':'MenuItem',
+                   'name':'menuFileExit',
+                   'label':'E&xit',
+                  },
+              ]
+             },
+         ]
+     },
+         'components': [
+
+{'type':'BitmapCanvas', 
+    'name':'canvas', 
+    'position':(0, 0), 
+    'size':(519, 403), 
+    'backgroundColor':(255, 255, 255), 
+    },
+
+] # end components
+} # end background
+] # end backgrounds
+} }
