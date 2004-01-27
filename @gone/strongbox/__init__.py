0fbc96cb8d1cabb9207c5d3d687d3e3710ba476d
sabren <sabren>
2004-01-27 16:21:49 +0000

    Added module.Classname to forward

--- @gone/strongbox/__init__.py
+++ @gone/strongbox/__init__.py
@@ -3,9 +3,21 @@ $Id$
 
 This package provides observable classes with static typing.
 """
+import warnings
 
 class StrongboxError(Exception): pass
-class forward: "(dummy class for defining recursive structures)"
+
+class forward:
+    """
+    dummy class for defining recursive structures)
+    does absolutely nothing.
+    """
+    def __init__(self, typename=None):
+        #@TODO: now that initialvalue passes instance,
+        #@TODO: make forward('Whatever') take a module.Class
+        #@TODO: and do some kind of morphing trick.
+        if type(typename) != str:
+            warnings.warn("forward() should be forward('module.ClassName')")
 
 from Observable import Observable
 from Attribute import Attribute as attr
