766cbbb00fbd6c322bc07176c7999dcebb3da55b
sabren <sabren>
2004-02-17 22:15:19 +0000

    *** empty log message ***

---
+++ crappyconda/sqlPair.py
@@ -0,0 +1,7 @@
+import MySQLdb
+
+dbc=MySQLdb.connect(
+                    user="pair",
+                    passwd="e76hrn",
+                    host="db.sabren.com",
+                    db="pair_db")
