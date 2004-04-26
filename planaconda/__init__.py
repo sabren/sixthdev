3e9067d07def645941a86dc59e4164dc092384bb
sabren <sabren>
2004-04-26 02:20:54 +0000

    10pm checkin

--- planaconda/__init__.py
+++ planaconda/__init__.py
@@ -4,6 +4,7 @@ from pytypes import FixedPoint
 import sixthday
 
 class Node(sixthday.Node):
+    ID = attr(int, default=0)
     data = attr(str)
     ntype = attr(str, okay=["project","goal","task"], default="task")
     estimate = attr(FixedPoint)
@@ -14,6 +15,9 @@ class Node(sixthday.Node):
     status = attr(str, default="unclear",
                   okay=["unclear", "to do", "in progress",
                         "complete", "rejected"])
+
+Node.__attrs__["parent"].type = Node
+Node.__attrs__["children"].type = Node
     
 
 class PlanApp(sixthday.AdminApp):
@@ -44,7 +48,21 @@ class PlanApp(sixthday.AdminApp):
         self.redirect("?action=viewNode&ID=%s" % node.ID)
 
     def viewNode(self):
-        return BoxView(self._getInstance(Node))
+        model = {}
+        nodeID = self.input.get("ID", "0")
+        if int(nodeID) == 0:
+            node = Node(name="root")
+            for n in self.clerk.match(Node):
+                if node.parent is None:
+                    node.children << n
+        else:
+            node = self._getInstance(Node)
+        model.update(BoxView(node))
+        model["crumbs"] = [BoxView(c) for c in node.crumbs]
+        return model
     
-    
+    def viewPlate(self):
+        nodes = self.clerk.match(Node, isOnPlate=True)
+        nodes.sort(lambda a,b: cmp(a.importance, b.importance))
+        return {"nodes": [BoxView(n) for n in nodes]} # @TODO: =~ listProject
 
--- planaconda/spec/PlanAppTest.py
+++ planaconda/spec/PlanAppTest.py
@@ -34,11 +34,43 @@ class PlanAppTest(unittest.TestCase):
         assert len(nodes) == 1
         self.assertEquals("post me", nodes[0].name)
 
-    def test_viewNode(self):
+    def test_viewNode_root(self):
+        
+        self.p.input["ID"] = 0
+        m = self.p.viewNode()
+        self.assertEquals("root", m["name"])
+        self.assertEquals(0, len(m["children"]))
+        
+        n = self.c.store(Node(name="abc"))
+
+        self.p.input["ID"] = 0
+        m = self.p.viewNode()
+        self.assertEquals("root", m["name"])
+        self.assertEquals(1, len(m["children"]))
+
+    def test_viewNode_node(self):
         n = self.c.store(Node(name="abc"))
         self.p.input["ID"] = n.ID
         m = self.p.viewNode()
         assert m["name"] == "abc"
+        self.assertEquals([], m["crumbs"])
+
+    def test_viewPlate(self):
+        # mmm... vowels...
+        self.c.store(Node(name="a", isOnPlate=True, importance=6))
+        self.c.store(Node(name="b"))
+        self.c.store(Node(name="c"))
+        self.c.store(Node(name="d"))
+        self.c.store(Node(name="e", isOnPlate=True, importance=3))
+        self.c.store(Node(name="f"))
+        self.c.store(Node(name="g"))
+        self.c.store(Node(name="h"))
+        self.c.store(Node(name="i", isOnPlate=True, importance=9))
+
+        m = self.p.viewPlate()
+        assert len(m["nodes"]) == 3
+        self.assertEquals(["e","a","i"],
+                          [n["name"] for n in m["nodes"]])
         
         
 if __name__=="__main__":
