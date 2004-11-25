3e0bc68bd9968815a2a87fc348e65c4153e63d7b
sabren <sabren>
2004-11-25 03:15:04 +0000

    *** empty log message ***

---
+++ @gone/platonic/__init__.py
@@ -0,0 +1,8 @@
+## platonic ################################################
+
+from Intercept import Intercept
+from Redirect import Redirect
+from Denial import Denial
+from Context import Context         
+from App import App
+from Model import Model
