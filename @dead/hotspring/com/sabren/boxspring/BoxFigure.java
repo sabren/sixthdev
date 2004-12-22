b19729166961daa2e081f839774f5976781eb1aa
sabren <sabren>
2004-12-22 10:39:02 +0000

    *** empty log message ***

---
+++ @dead/hotspring/com/sabren/boxspring/BoxFigure.java
@@ -0,0 +1,27 @@
+/*
+ * Created on Dec 9, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.sabren.boxspring;
+
+import org.jhotdraw.figures.TextFigure;
+
+/**
+ * @author michal
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class BoxFigure extends TextFigure {
+
+	/**
+	 * 
+	 */
+	public BoxFigure(String text) {
+		super();
+		setText(text);
+	}
+	
+}
