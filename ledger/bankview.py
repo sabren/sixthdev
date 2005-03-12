a5fbef13b56c45662a5ef9ee53516c55c154d7cc
sabren <sabren>
2005-03-12 19:41:51 +0000

    fixed the regular expression

--- ledger/bankview.py
+++ ledger/bankview.py
@@ -1,7 +1,7 @@
 import re, sys
 
 
-e = re.compile(r"(\d{4}/)(\d{2}/\d{2})(.*){(\d{2}/\d{2})}")
+e = re.compile(r"(\d{4}/)(\d{2}/\d{2})(.*){(\d{4}/\d{2}/\d{2})}")
 
 for line in sys.stdin:
     sys.stdout.write(e.sub(r"\1\4\3",line))
