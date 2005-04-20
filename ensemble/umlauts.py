0969ec03ec9da5fa266a7a0f5c10726ae01ab00e
sabren <sabren>
2005-04-20 06:20:46 +0000

    called script directly

--- ensemble/umlauts.py
+++ ensemble/umlauts.py
@@ -1,6 +1,6 @@
 
 import ensemble
-perl=ensemble.Director("perl -w ensemble.pl")
+perl=ensemble.Director("./ensemble.pl")
 perl.loadModule("Acme::Umlautify", "acme")
 print perl.acme.umlautify("What we need more of is umlauts.")
 
