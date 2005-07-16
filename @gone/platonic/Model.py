063b205329302721275391ecf47d222b6e3b3350
sabren <sabren>
2005-07-16 16:52:47 +0000

    added attribute-style access

--- @gone/platonic/Model.py
+++ @gone/platonic/Model.py
@@ -6,3 +6,9 @@ class Model(dict):
     def __init__(self, **kwargs):
         super(Model, self).__init__()
         self.update(kwargs)
+
+    def __getattr__(self, key):
+        """
+        this lets you access via . instead of []
+        """
+        return self[key]
