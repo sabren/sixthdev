76d853cebaf103744dc2db84b84f583c8b2c5bcb
sabren <sabren>
2005-08-17 21:44:08 +0000

    implemented simple, nonrecursive SolutionBuilder

--- trailblazer/BlazeHound.py
+++ trailblazer/BlazeHound.py
@@ -1,7 +1,188 @@
 
-#* BlazeHound solution compiler
-#** import saxophone
-#** trail:blaze
-#** trail:extend
-#** trail:patch
+# This is BlazeHound, our XML-based solution compiler.
 
+# Here's what we're shooting for: We should be able
+# to put a bunch of ideas in the same file and have it
+# all mixed up, and modify things as we go along so
+# that we're able to explain things in nice little
+# pieces... But then in the end we want the computer
+# to be able to go back and compile all the trails into
+# the full solution
+_TEST_NARRATIVE =\
+"""\
+<xml>
+  <trail:blaze trail="number">123</trail:blaze>
+  <trail:blaze trail="alpha">XYZ</trail:blaze>
+  <trail:replace trail="alpha">ABC</trail:replace>
+  <trail:extend trail="number">456</trail:extend>
+  <trail:extend trail="alpha">DEF</trail:extend>
+</xml>\
+"""
+_TEST_SOLUTION_ALPHA ="ABCDEF"
+_TEST_SOLUTION_NUMBER ="123456"
+
+
+# It's pretty straightforward. We're just going
+# to parse the XML file and add all the chunks
+# to a solution. So in terms of our technology,
+# we want to create a Saxophone with some TagHandlers
+# that build a Solution object from the xml tags.
+from Solution import Solution
+from altosax import Saxophone, TagHandler, DOCUMENT
+
+# Since state machines are kind of tricky, we can
+# use the logging module to help us see what's
+# happening. 
+import logging
+
+# To see the output, just uncomment this next line:
+#logging.root.setLevel(logging.DEBUG)
+
+
+# Just to be clear we have three tags. We're
+# going to skirt around the whole issue of xml
+# namespaces here, because by default, xml.sax
+# just ignores the colon and returns the whole
+# qname. This means "trail:" isn't a real
+# namespace. This should probably be fixed later.
+
+#@TODO: clean up namespace handling
+TAG_BLAZE = "trail:blaze"
+TAG_REPLACE = "trail:replace"
+TAG_EXTEND = "trail:extend"
+
+# Now the basic TagHandler almost does what we want,
+# but not quite. Why? Because it's appending to a
+# list as we go along, but what we really want to
+# do is append to a Solution. But that's easy:
+
+class SolutionBuilder(TagHandler):
+    def __init__(self, tag, attrs):
+        super(SolutionBuilder, self).__init__(tag, attrs)
+        self.data = Solution()
+
+# Now, we have three tags we need to handle: blaze,
+# extend, and replace. We could implement each one as
+# a TagHandler and pass the same Solution object around
+# but since Solution is a recursive data structure anyway,
+# it's easier to just use SolutionBuilder.
+
+# What I'm proposing here is that each tag gets its
+# own Solution object, and as we close the tags, we
+# just append those Solutions to the parent solution.
+
+# This implies that you can nest the "blaze" tags
+# (because creating a new solution is always okay)
+# but you nesting the extend or replace tags is
+# tricky because a child tag has no access to the
+# parent tag's data. All we can do is tell the parent
+# that this is an "extend" or "replace" action once
+# the tag gets closed, and here's all the new data
+# we collected. In other words, all the magic happens
+# in the SolutionBuilder.child handler:
+
+    def child(self, child):
+        if child.tag == TAG_BLAZE:
+            self.onBlaze(child)
+        elif child.tag == TAG_EXTEND:
+            self.onExtend(child)
+        elif child.tag == TAG_REPLACE:
+            self.onReplace(child)
+        elif self.tag == DOCUMENT:
+            self.data = child.data
+        else:
+            # not a trail tag so we can just collapse the
+            # solution into a string:
+            self.data.append(str(child))
+
+############################################################
+#@TODO: TagHandler should do dispatch
+############################################################
+#
+# I REALLY don't like that.
+#
+# The whole point of saxophone was that I wouldn't have to
+# write my own dispatch routine like that. It was supposed
+# to happen through composition and a generic dispatch and
+# callback mechanism. What went wrong?
+#
+# Well, right now the dispatch happens when the tag STARTS,
+# and it's all controlled centrally in the Saxophone. What
+# I want here is to handle different types of tags when
+# they CLOSE. So I missed the boat a little bit. I THINK
+# I'd like to keep the on-open stuff for zebra/reptile,
+# but I'll have to think about it. In other words, I've
+# either got it backwards or I could need either, depending
+# on the situation. But whatever. I'll try again with
+# a different design some other time. The above dispatch
+# will work just fine for now.
+#
+############################################################            
+
+# Anyway, the mini tag handler-like methods are easy
+# enough. Here's the Blaze handler:
+
+    def onBlaze(self, child):
+        # @TODO: prevent duplicate trail names on blaze?
+        trail = child.attrs["trail"]
+        logging.debug("blazing %s" % trail)
+        self.data.blaze(trail)
+        # @TODO: We really need a Solution.extend() here.
+        # The .append() method works fine for recursive
+        # iteration but what about nested blazes? I don't
+        # think you can follow across an "anonymous" sub
+        # solution like that, so any blazes in the child
+        # would be lost with .append()... This is only an
+        # issue if we want recursive blazes after all, so
+        # I'm not sure it matters. Meanwhile, here's a
+        # compromise:
+        # (I may extract this later to be Solution.extend)
+        for item in child.data:
+            logging.debug("appending %s at %s" % (item, trail))
+            self.data.at(trail).append(item)
+
+# The extend tag works the same way.
+# I guess we probably do need extend, huh?
+
+    def onExtend(self, child):
+        trail = child.attrs["trail"]
+        for item in child.data:
+            logging.debug("appending %s at %s" % (item, trail))
+            self.data.at(trail).append(item)
+            
+# Replace also works this way, but clears the trail first.
+# Only I don't think this is going to work either because
+# we don't have the entire tree.
+#
+# GAH. Doing all this in child() seems so much more
+# elegant than passing in the parent data to each tag,
+# but it looks like we need that data after all if we
+# want to use dotted trail names in extend and replace.
+# ugh. 
+
+    def onReplace(self, child):
+        trail = child.attrs["trail"]
+        logging.debug("clearing %s" % trail)
+        self.data.at(trail).clear()
+        for item in child.data:
+            logging.debug("appending %s at %s" % (item, trail))
+            self.data.at(trail).append(item)
+    
+
+# so really this should all work for our test but
+# not yet for dotted paths. I'll have to rethink
+# this. But let's go ahead and implement the test:
+import unittest 
+class SolutionBuilderTest(unittest.TestCase):
+    def test(self):
+        sxp = Saxophone(SolutionBuilder)
+        sol = sxp.parseString(_TEST_NARRATIVE).data
+        logging.debug(sol.getBlazes())
+        logging.debug(list(sol))
+        self.assertEquals(str(sol["alpha"]), _TEST_SOLUTION_ALPHA)
+        self.assertEquals(str(sol["number"]), _TEST_SOLUTION_NUMBER)      
+
+# run the tests
+if __name__=="__main__":
+    unittest.main()
+    
