029d912c6edb3052d5cda3ff6d6a5ea953d4d53a
sabren <sabren>
2005-09-21 01:10:45 +0000

    simple version : pure sax

--- trailblazer/BlazeHound.py
+++ trailblazer/BlazeHound.py
@@ -1,241 +1,271 @@
-
-# This is BlazeHound, our XML-based solution compiler.
-
-# Here's what we're shooting for: We should be able
-# to put a bunch of ideas in the same file and have it
-# all mixed up, and modify things as we go along so
-# that we're able to explain things in nice little
-# pieces... But then in the end we want the computer
-# to be able to go back and compile all the trails into
-# the full solution
-_TEST_NARRATIVE =\
-"""\
-<xml>
-  <trail:blaze trail="char">
-    <trail:blaze trail="alpha">XYZ</trail:blaze>
-    <trail:blaze trail="number">123</trail:blaze>
-  </trail:blaze>
-  <trail:replace trail="char.alpha">ABC</trail:replace>
-  <trail:extend trail="char.number">456</trail:extend>
-  <trail:extend trail="char.alpha">DEF</trail:extend>
-</xml>\
 """
-_TEST_SOLUTION_CHAR ="ABCDEF123456"
-_TEST_SOLUTION_ALPHA ="ABCDEF"
-_TEST_SOLUTION_NUMBER ="123456"
+This is BlazeHound, our XML-based solution compiler.
+
+Here's what we're shooting for: We should be able
+to put a bunch of ideas in the same file and have it
+all mixed up, and modify things as we go along so
+that we're able to explain things in nice little
+pieces... But then in the end we want the computer
+to be able to go back and compile all the trails into
+the full solution, dumping the tests to one place,
+the final code to another, and the docs somewhere else.
+so basically we want to parse an xml file and build
+a Solution objects
+"""
+
+import unittest
+class BlazeHoundTest(unittest.TestCase):
+    def test(self):
+        hound = BlazeHound()
+        sol = hound.parseString(
+        '''\
+        <xml>
+          <trail:blaze trail="char">
+            <trail:blaze trail="alpha">XYZ</trail:blaze>
+            <trail:blaze trail="number">123</trail:blaze>
+          </trail:blaze>
+          <trail:extend trail="char.number">456</trail:extend>
+          <trail:replace trail="char.alpha">ABC</trail:replace>
+          <trail:extend trail="char.alpha">DEF</trail:extend>
+        </xml>\
+        ''')
+        logging.debug(sol.getBlazes())
+        logging.debug(list(sol))
+
+        # so the alpha and number areas should be easy:
+        self.assertEquals("ABCDEF", str(sol["char.alpha"]))
+        self.assertEquals("123456", str(sol["char.number"]))
+
+        # and if we clear out the whitespace:
+        self.assertEquals("ABCDEF123456",
+                          "".join(str(sol["char"]).split()))
 
 
-# It's pretty straightforward. We're just going
-# to parse the XML file and add all the chunks
-# to a solution. So in terms of our technology,
-# we want to create a Saxophone with some TagHandlers
-# that build a Solution object from the xml tags.
+
+"""
+So the idea is pretty straightforward. We're just going
+to parse the XML file and add all the chunks
+to a Solution. So in terms of our technology,
+we want to create a Saxophone with some TagHandlers
+that build a Solution object from the xml tags.
+"""
 from Solution import Solution
 from altosax import Saxophone, TagHandler, DOCUMENT
 
-# Since state machines are kind of tricky, we can
-# use the logging module to help us see what's
-# happening. 
+"""
+Since state machines are kind of tricky, we can
+use the logging module to help us see what's
+happening.
+"""
 import logging
 
-# To see the output, just uncomment this next line:
+"""
+To see the log output, just uncomment this next line:
+"""
 #logging.root.setLevel(logging.DEBUG)
 
 
-# Just to be clear we have three tags. We're
-# going to skirt around the whole issue of xml
-# namespaces here, because by default, xml.sax
-# just ignores the colon and returns the whole
-# qname. This means "trail:" isn't a real
-# namespace. This should probably be fixed later.
+"""
+So... Our XML format has three tags. We're
+going to skirt around the whole issue of xml
+namespaces here, because by default, xml.sax
+just ignores the colon and returns the whole
+qualified name as a string. This means "trail:"
+isn't a real xml namespace. That should probably
+be fixed later.
+"""
 
 #@TODO: clean up namespace handling
 TAG_BLAZE = "trail:blaze"
-TAG_REPLACE = "trail:replace"
 TAG_EXTEND = "trail:extend"
+TAG_REPLACE = "trail:replace"
 
-# Now the basic TagHandler almost does what we want,
-# but not quite. Why? Because it's appending to a
-# list as we go along, but what we really want to
-# do is append to a Solution.
+"""
+The basic TagHandler class almost does what we
+want, but not quite. Why? Because it's appending
+to a list as we go along, but what we really want
+to do is append to a Solution.
 
-# ah, but which solution? if all we had were blaze
-# tags, then we could just make up a new solution
-# at the start of each tag and build up the tree
-# using anonymous instances.
+ah, but which Solution? if all we had were blaze
+tags, then we could just make up a new Solution
+at the start of each tag and build up the tree
+using anonymous instances.
 
-# but, since we want to be able to modify the tree
-# as we go (using the replace and extend tags)
-# then we need to be able to walk the tree. which
-# means we need to hold on to the root.
+But: since we want to be able to modify the tree
+as we go (using the replace and extend tags)
+then we need to be able to walk the tree at any
+time. which means we need to keep a reference
+to the root Solution in a variable somewhere.
 
-# it also means that we can't have a separate
-# solution for each tag.
+it also means that we can't have an anonymous
+Solution for each tag, which would have been
+easier. We need to add each child Solution to
+its parent immediately when we start the
+trail:blaze tag.
 
-# if we want nested blaze tags, then we need the
-# concept of a "current" solution as well as the
-# "root" solution. so we need exactly one root
-# Solution, plus one Solution per trail: tag.
+Also, if we want nested blaze tags, then we
+need the concept of a "current" solution as
+well as the "root" solution. so we need exactly
+one root Solution, plus a stack, containing one
+Solution per trail tag. (We use a stack because
+the tags can be nested). Finally, we need to
+keep track of which one is the current solution.
 
-# But that's all easy os far as SolutionBuilder
-# is concerned. just replace the .data attribute
-# with a Solution, either a specific one that
-# we pass in, or a brand new one:
-
-class SolutionBuilder(TagHandler):
-    def __init__(self, tag, attrs, solution):
-        super(SolutionBuilder, self).__init__(tag, attrs)
-        self.data = solution
-
-# by default, all tags and data (for example, html content)
-# will go to this generic tog. 
-
-# But, we have three tags we need to handle specially:
-# blaze, extend, and replace. We could implement each one as
-# a TagHandler and pass the same Solution object around
-# but since Solution is a recursive data structure anyway,
-# it's easier to just use SolutionBuilder.
-
-# What I'm proposing here is that each tag gets its
-# own Solution object, and as we close the tags, we
-# just append those Solutions to the parent solution.
-
-# This implies that you can nest the "blaze" tags
-# (because creating a new solution is always okay)
-# but you nesting the extend or replace tags is
-# tricky because a child tag has no access to the
-# parent tag's data. All we can do is tell the parent
-# that this is an "extend" or "replace" action once
-# the tag gets closed, and here's all the new data
-# we collected. In other words, all the magic happens
-# in the SolutionBuilder.child handler:
-
-    def child(self, child):
-        if child.tag == TAG_BLAZE:
-            self.onBlaze(child)
-        elif child is not None:
-            # not a trail tag so we can just collapse the
-            # solution into a string:
-            self.data.append(str(child))
-
-############################################################
-#@TODO: TagHandler should do dispatch
-############################################################
-#
-# I REALLY don't like that.
-#
-# The whole point of saxophone was that I wouldn't have to
-# write my own dispatch routine like that. It was supposed
-# to happen through composition and a generic dispatch and
-# callback mechanism. What went wrong?
-#
-# Well, right now the dispatch happens when the tag STARTS,
-# and it's all controlled centrally in the Saxophone. What
-# I want here is to handle different types of tags when
-# they CLOSE. So I missed the boat a little bit. I THINK
-# I'd like to keep the on-open stuff for zebra/reptile,
-# but I'll have to think about it. In other words, I've
-# either got it backwards or I could need either, depending
-# on the situation. But whatever. I'll try again with
-# a different design some other time. The above dispatch
-# will work just fine for now.
-#
-############################################################            
-
-# Anyway, the mini tag handler-like methods are easy
-# enough. Here's the Blaze handler:
-
-    def onBlaze(self, child):
-        trail=child.attrs["trail"]
-        self.data.blaze(trail)
-        self.data.at(trail).extend(child.data)
-
-    def child(self, data):
-        ## self.data.append(data)
-        pass
-
-# so really this should all work for our test but
-# not yet for dotted paths. I'll have to rethink
-# this. But let's go ahead and implement the test:
-import unittest 
-class BlazeHoundTest(unittest.TestCase):
-    def test(self):
-        sxp = BlazeHound()
-        sol = sxp.parseString(_TEST_NARRATIVE).data
-        logging.debug(sol.getBlazes())
-        logging.debug(list(sol))
-        print sxp.getSolution()
-        self.assertEquals(str(sol["chars.alpha"]), _TEST_SOLUTION_ALPHA)
-        self.assertEquals(str(sol["chars.number"]), _TEST_SOLUTION_NUMBER)      
+So:
+"""
+import xml.sax
+class BlazeHound(xml.sax.ContentHandler):
+    def __init__(self):
+        xml.sax.ContentHandler.__init__(self)
+        self.root = Solution()     # root Solution 
+        self.stack = []            # child Solutions
+        self.current = self.root   # current Solution
 
 
+    """
+    Now, the way sax works, there are certain methods that
+    get called as you parse from the top of the xml file
+    to the bottom, one for each xml concept that you
+    encounter.
 
+    So character data, whitespace, and xml entities are
+    pretty easy. We just pass the data directly to the
+    current Solution:
+    """
 
-
-###############################################################
-
-class BlazeHoundTag(SolutionBuilder):
-    def __init__(self, hound, tag, attrs):
-        solution = Solution()
-        super(BlazeHoundTag, self).__init__(tag, attrs, solution)
-        self.hound = hound
-        self.hound.pushContext(solution)
-        self.trail = attrs["trail"]
-
-
-class ExtendTag(BlazeHoundTag):
-    def close(self):
-        self.hound.popContext()
-        self.hound.getSolution().at(self.trail).extend(self.data)
-        return None
+    # sax content events:
     
-class ReplaceTag(BlazeHoundTag):
-    def close(self):
-        self.hound.popContext()
-        print self.hound.getSolution()
-        self.hound.getSolution().at(self.trail).clear()
-        self.hound.getSolution().at(self.trail).extend(self.data)
-        return None
+    def characters(self, content):
+        self.current.append(content)
 
-class BlazeHound(Saxophone):
+    def ignorableWhitespace(self, whitespace):
+        self.current.append(whitespace)
 
-    def __init__(self, sol=None):
-        # by default, use self.onAny, below
-        super(BlazeHound, self).__init__(self.onAny)
-
-        # the other tags are easy:
-        self.onTag(TAG_EXTEND,
-                   lambda tag, attrs: ExtendTag(self, tag, attrs))
-        self.onTag(TAG_REPLACE,
-                   lambda tag, attrs: ReplaceTag(self, tag, attrs))
+    def skippedEntity(self, name):
+        # handle entities like &amp;
+        self.current.append("&%s;" % name)
 
 
-# now, we need to be able to get to our solution as we go along
-# this means we can't wait for the top level tag to close in order
-# to get at it. so... we need to create the root solution
-# ourselves and hold on to it.
+    """
+    Most of the tags should also just pass through
+    unchanged to the Solution, but we need to set
+    up some special handlers for the three tags
+    that we defined earlier.
 
-        self.rootSolution = sol or Solution()
-        self.contexts = [self.rootSolution] # solution stack
+    So, we'll just hard code some dispatch logic
+    for these, and fill them in later:
+    """
 
-# Saxophone.byDefault needs to be a callable that returns
-# a TagHandler. There's no reason it con't just be a method:
+    def startElement(self, name, attrs):
+        method = {
+            TAG_BLAZE   : self.tag_blaze,
+            TAG_EXTEND  : self.tag_extend,
+            TAG_REPLACE : self.tag_replace,
+        }.get(name)
+        if method:
+            method(attrs)
+        else:
+            self.tag_normal(name, attrs)
 
-    def onAny(self, tag, attrs):
-        return SolutionBuilder(tag, attrs, self.getContext())
+    """
+    All these special tag handlers are going to push
+    the current solution onto the stack, and then pick
+    a new solution to be the current one.
+    
+    After we close a special tag, we just go back to
+    working with the previous Solution. All three 
+    special end tags will do the same thing, so we
+    can just take care of all of them at once:
+    """
 
-    def getSolution(self):
-        return self.rootSolution
+    def endElement(self, name):
+        method = {
+            TAG_BLAZE   : self.end_special,
+            TAG_REPLACE : self.end_special,
+            TAG_EXTEND  : self.end_special,
+        }.get(name)
+        if method:
+            method()
+        else:
+            self.end_normal(name)
+
+    def end_special(self):
+        self.current = self.stack.pop()
 
 
-    def getContext(self):
-        return self.contexts[-1]
+    """
+    Normal tags just pass through directly. We won't
+    always get the exact same start tags since the
+    attributes are turned into python dicts, which 
+    are unordered. Similarly, <empty/> tags will be
+    turned into <empty></empty> tag pairs, but it
+    doesn't matter: the xml is equivalent.
+    """
 
-    def pushContext(self, solution):
-        self.contexts.append(solution)
+    def tag_normal(self, name, attrs):
+        self.current.append("<%s" % name)
+        for k,v in attrs.items():
+            self.current.append(' %s="%s"' % (k, v))
+        self.current.append(">")
 
-    def popContext(self):
-        return self.contexts.pop()
+    def end_normal(self, name):
+        self.current.append("</%s>" % name)
+
+
+    """    
+    Now we can consider the special tags individually.
+    
+    The blaze tag starts a new Solution, so we need
+    to push the current solution onto the stack
+    and create a new one:
+    """
+
+    def tag_blaze(self, attrs):
+        trail = attrs["trail"]
+        # create the new child solution:
+        self.current.blaze(trail)
+        # push this one onto the stack
+        self.stack.append(self.current)
+        # and replace it with the new child
+        self.current = self.current[trail]
+
+
+    """
+    The extend tag jumps back to an existing solution:
+    """
+   
+    def tag_extend(self, attrs):
+        trail = attrs["trail"]
+        # push this one onto the stack
+        self.stack.append(self.current)
+        # and replace it with the trail
+        # by following the dots from the root:
+        self.current = self.root[trail]
+
+
+    """
+    And the replace tag does the exact same thing, but
+    it clears the previous solution first. Since the
+    """
+
+    def tag_replace(self, attrs):
+        self.root[attrs["trail"]].clear()
+        self.tag_extend(attrs)
+
+
+    """
+    And... That's really all we need.
+
+    Here are two convenience methods though:
+    """
+    def parse(self, filename_or_stream):
+        xml.sax.parse(filename_or_stream, self)
+        return self.root
+
+    def parseString(self, string):
+        xml.sax.parseString(string, self)
+        return self.root
+    
 
 # run the tests
 if __name__=="__main__":
