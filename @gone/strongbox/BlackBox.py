267ee0b242a9e8d7c438f3d55b8aafbc8a4e0510
sabren <sabren>
2006-01-20 18:23:18 +0000

    stop infinite loop, commented out pickling

--- @gone/strongbox/BlackBox.py
+++ @gone/strongbox/BlackBox.py
@@ -1,4 +1,4 @@
-from strongbox import MetaBox, Private
+from strongbox import MetaBox, Private, Attribute
 
 # I thought about factoring out a
 # BaseBox... But I just couldn't
@@ -28,6 +28,27 @@ class BlackBox(object):
         pass
 
     def __repr__(self):
+        # for linksets and in other cases where objects refer
+        # back to each other, this could create an infinite loop,
+        # so we only show plain attributes.
         return "%s(%s)" % (self.__class__.__name__ ,
                            ", ".join(["%s=%s" % (a, getattr(self, a))
-                                      for a in self.__attrs__]))
+                                      for a,v in self.__attrs__.items()
+                                      if v.__class__ is Attribute]))
+    
+
+##    ## this stuff is for pickling... but it turns out pickling
+##    ## is a can of worms when you have injectors lying around.
+##    ## I think maybe you can use pickle OR clerk but not both.
+##    ## I haven't tested that theory though.
+##   
+##     def memento(self):
+##         res = {}
+##         for a in self.__attrs__:
+##             res[a]=getattr(self,a)
+##         return res
+##     def __getstate__(self):
+##         return self.memento()
+##     def __setstate__(self, memento):
+##         self.__private__()
+##         self.update(**memento)
