9bd09787c98ff343aaa246113318d7835abb5799
sabren <sabren>
2007-05-02 16:43:05 +0000

    un-hardcoded the filename

--- ledger/amex.py
+++ ledger/amex.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python2.5
 import decimal
+import sys
 
 # import pdb; pdb.set_trace()
 
@@ -10,24 +12,29 @@ def fixdate(d):
     return "/".join([y,m,d])
 
 
-for line in open("amex2006.csv").read().split("\n"):
 
-    if line.startswith("Payee Location"):
-        real_date = fixdate(line.split(",")[-1])
+if __name__=="__main__":
 
-    if line.startswith('"4100320225"'):
-        data = line.split(",")
-        init_date = fixdate(unquote(data[2]))
+    stream = sys.stdin if len(sys.argv)==1 else open(sys.argv[1])
 
-        print "%s amex {%s}" % ( init_date, real_date)
+    for line in stream.read().split("\n"):
 
-    if line.startswith("Totals"):
-        data = line.split(",")
-        whatIgot=unquote(data[-1])
-        whatTheyTook=unquote(data[-3])
-        whatWeStartedWith=unquote(data[-5])
+        if line.startswith("Payee Location"):
+            real_date = fixdate(line.split(",")[-1])
 
-        print "    asset:checking %33s" % whatIgot
-        print "    expense:fees:amex %30s" % whatTheyTook
-        print "    asset:merchant %33s" % -decimal.Decimal(whatWeStartedWith)
+        if line.startswith('"4100320225"'):
+            data = line.split(",")
+            init_date = fixdate(unquote(data[2]))
+
+            print "%s amex {%s}" % ( init_date, real_date)
+
+        if line.startswith("Totals"):
+            data = line.split(",")
+            whatIgot=unquote(data[-1])
+            whatTheyTook=unquote(data[-3])
+            whatWeStartedWith=unquote(data[-5])
+
+            print "    asset:checking %33s" % whatIgot
+            print "    expense:fees:amex %30s" % whatTheyTook
+            print "    asset:merchant %33s" % -decimal.Decimal(whatWeStartedWith)
 
