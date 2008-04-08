fa796ac9059625fc8a1a4e6e3d9610c4efe7f97b
sabren <sabren>
2008-04-08 15:50:33 +0000

    switched to csv module to avoid errors with amounts greater than ,000

--- ledger/amex.py
+++ ledger/amex.py
@@ -1,6 +1,7 @@
+
 #!/usr/bin/python2.5
 """
-Log into americanexpress.com/merchant/
+Log into https://www.americanexpress.com/merchant/
 
 click view your statement
 change date range if you want
@@ -23,39 +24,54 @@ and copy/paste the transactions into the ledger.
 
 import decimal
 import sys
-
+import csv
 
 # import pdb; pdb.set_trace()
 
-def unquote(x):
-    return x.replace('"','').replace("$","")
+def cleanup(x):
+    # strip out commas and dollar signs in the numbers
+    return x.replace(',','').replace("$","")
 
 def fixdate(d):
     m,d,y = d.split("/")
     return "/".join([y,m,d])
 
 
+def parseline(line):
+    """
+    cant use csv because not all lines in
+    the 
+    """
+
 
 if __name__=="__main__":
 
     stream = sys.stdin if len(sys.argv)==1 else open(sys.argv[1])
+    reader = csv.reader(stream)
 
-    for line in stream.read().split("\n"):
+    firstTotal = True
 
-        if line.startswith("Payee Location"):
-            real_date = fixdate(line.split(",")[-1])
+    for line in reader:
 
-        if line.startswith('"4100320225"'):
-            data = line.split(",")
-            init_date = fixdate(unquote(data[2]))
+        if not line: continue
 
+        if line[0] == "Payee Location:":
+            real_date = fixdate(line[-1])
+
+        elif line[0] == '4100320225':
+            init_date = fixdate(line[2])
             print "%s=%s amex" % ( init_date, real_date)
 
-        if line.startswith("Totals"):
-            data = line.split(",")
-            whatIgot=unquote(data[-1])
-            whatTheyTook=unquote(data[-3])
-            whatWeStartedWith=unquote(data[-5])
+        elif line[0] == "Totals":
+
+            # the first one is a summary. skip it.
+            if firstTotal:
+                firstTotal = False
+                continue
+            
+            whatIgot=cleanup(line[-1])
+            whatTheyTook=cleanup(line[-3])
+            whatWeStartedWith=cleanup(line[-5])
 
             print "    asset:checking %33s" % whatIgot
             print "    expense:fees:amex %30s" % whatTheyTook
