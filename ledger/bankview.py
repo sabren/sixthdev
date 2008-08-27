65feba1af453f2db87a0fdd1a33e300dda3d95da
sabren <sabren>
2008-08-27 00:09:02 +0000

    simplified

--- ledger/bankview.py
+++ ledger/bankview.py
@@ -1,14 +1,8 @@
 #!/usr/local/bin/python2.5
 import sys
-import ledger
+from ledger import bankView, parseLedger
 
-#e = re.compile(r"(\d{4}/\d{2}/\d{2})(.*){(\d{4}/\d{2}/\d{2})}")
-#
-#for line in sys.stdin:
-#    sys.stdout.write(e.sub(r"\3\2",line))
-    
-book = ledger.parseLedger(sys.stdin.read())
-book = ledger.bankView(book)
+stream = sys.stdin
 
-for item in book:
-    print item
+for entry in bankView(parseLedger(stream.read())):
+    print entry
